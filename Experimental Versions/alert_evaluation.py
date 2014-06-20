2faa0e4f702a61fc8e01488cdd892049b1f125ec
piereluya@gmail.com <piereluya@gmail.com@69feadb2-5f6b-d0c9-4c76-7ef1b510d1ee>
2014-06-20 03:14:18 +0000

    - corrected minor bugs

--- Experimental Versions/alert_evaluation.py
+++ Experimental Versions/alert_evaluation.py
@@ -30,12 +30,12 @@ def across_axis_alert(xz_tilt, xy_tilt, xz_vel, xy_vel, num_nodes, T_disp, T_vel
     ac_ax=pd.DataFrame(data=None, index=range(1,num_nodes+1))
 
     #evaluating net displacements within real-time window
-    ac_ax['xz_disp']=xz_tilt[-1].values-xz_tilt[0].values
-    ac_ax['xy_disp']=xy_tilt[-1].values-xy_tilt[0].values
+    ac_ax['xz_disp']=np.round(xz_tilt.values[-1]-xz_tilt.values[13], 3)
+    ac_ax['xy_disp']=np.round(xy_tilt.values[-1]-xy_tilt.values[13], 3)
 
     #checking if displacement threshold is exceeded in either axis
-    ac_ax['disp_alert']=np.where(np.abs(ac_ax['xz_disp'].values)>T_disp or
-                                 np.abs(ac_ax['xy_disp'].values)>T_disp,
+    cond = np.abs(ac_ax['xz_disp'].values)>T_disp, np.abs(ac_ax['xy_disp'].values)>T_disp
+    ac_ax['disp_alert']=np.where(np.any(cond),
 
                                  #disp alert=1
                                  np.ones(len(ac_ax)),
@@ -44,36 +44,36 @@ def across_axis_alert(xz_tilt, xy_tilt, xz_vel, xy_vel, num_nodes, T_disp, T_vel
                                  np.zeros(len(ac_ax)))
 
     #getting minimum axis velocity value
-    ac_ax['min_vel']=np.where(np.abs(xz_vel[-1].values)<np.abs(xy_vel[-1].values),
-                              np.abs(xz_vel[-1].values),
-                              np.abs(xy_vel[-1].values))
+    ac_ax['min_vel']=np.round(np.where(np.abs(xz_vel.values[-1])<np.abs(xy_vel.values[-1]),
+                                       np.abs(xz_vel.values[-1]),
+                                       np.abs(xy_vel.values[-1])), 4)
 
     #getting maximum axis velocity value
-    ac_ax['max_vel']=np.where(np.abs(xz_vel[-1].values)>=np.abs(xy_vel[-1].values),
-                              np.abs(xz_vel[-1].values),
-                              np.abs(xy_vel[-1].values))
+    ac_ax['max_vel']=np.round(np.where(np.abs(xz_vel.values[-1])>=np.abs(xy_vel.values[-1]),
+                                       np.abs(xz_vel.values[-1]),
+                                       np.abs(xy_vel.values[-1])), 4)
                                 
     #checking if velocity threshold is exceeded
                         #checking if proportional velocity is present across node
-    ac_ax['vel_alert']=np.where(ac_ax['min_vel'].vlues/ac_ax['max_vel'].values<k_ac_ax,   
+    ac_ax['vel_alert']=np.where(ac_ax['min_vel'].values/ac_ax['max_vel'].values<k_ac_ax,   
 
                                 #vel alert=0
                                 np.zeros(len(ac_ax)),    
 
                                 #checking if max node velocity exceeds threshold velocity for alert 1
-                                np.where(ac_ax['max_va'].values<=TvelA1,                  
+                                np.where(ac_ax['max_vel'].values<=T_velA1,                  
 
                                          #vel alert=0
                                          np.zeros(len(ac_ax)),
 
                                          #checking if max node velocity exceeds threshold velocity for alert 2
-                                         np.where(ac_ax['max_va'].values<=TvelA2,         
+                                         np.where(ac_ax['max_vel'].values<=T_velA2,         
 
                                                   #vel alert=1
-                                                  np.ones(len(ax_Ax)),
+                                                  np.ones(len(ac_ax)),
 
                                                   #vel alert=2
-                                                  2*np.ones(len(ax_Ax)))))
+                                                  2*np.ones(len(ac_ax)))))
 
     
     ac_ax['node_alert']=np.where(ac_ax['vel_alert'].values==0,
