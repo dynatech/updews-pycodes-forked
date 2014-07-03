3a1ad4d25d856eab508bcaf7ee64f187080f44cc
ricsatjr@gmail.com <ricsatjr@gmail.com@69feadb2-5f6b-d0c9-4c76-7ef1b510d1ee>
2014-07-03 02:52:44 +0000

    - more edits

--- Experimental Versions/generate_proc_rt.py
+++ Experimental Versions/generate_proc_rt.py
@@ -1,82 +1,20 @@
 from datetime import datetime, date, time, timedelta
 import pandas as pd
+from pandas.stats.api import ols
 import numpy as np
+import matplotlib.pyplot as plt
 
 import generic_functions as gf
 
-def get_rt_window(rt_window_length,roll_window_size):
+
+
+
+
+
+def zero_plot(df,ax,off):
+    df0=df-df.loc[(df.index==df.index[0])].values.squeeze()
+    df0.plot(ax=ax,marker='.',legend=False)
     
-    ##DESCRIPTION:
-    ##returns the time interval for real-time monitoring
-
-    ##INPUT:
-    ##rt_window_length; float; length of real-time monitoring window in days
-    ##roll_window_size; integer; number of data points to cover in moving window operations
-    
-    ##OUTPUT: 
-    ##end, start, offsetstart; datetimes; dates for the end, start and offset-start of the real-time monitoring window 
-
-    ##set current time as endpoint of the interval
-    end=datetime.now()
-
-    ##round down current time to the nearest HH:00 or HH:30 time value
-    end_Year=end.year
-    end_month=end.month
-    end_day=end.day
-    end_hour=end.hour
-    end_minute=end.minute
-    if end_minute<30:end_minute=0
-    else:end_minute=30
-    end=datetime.combine(date(end_Year,end_month,end_day),time(end_hour,end_minute,0))
-
-    #starting point of the interval
-    start=end-timedelta(days=rt_window_length)
-    
-    #starting point of interval with offset to account for moving window operations 
-    offsetstart=end-timedelta(days=rt_window_length+((roll_window_size+1)/48.))
-    
-    return end, start, offsetstart
-
-
-def nodes_to_columns(df, num_nodes):
-    #creating series list
-    xzlist=[]
-    xylist=[]
-    #xlist=[]
-    for curnodeID in range(num_nodes):
-        #extracting data from current node, with good tilt filter
-        df_curnode=df[(df.Node_ID==curnodeID+1)]
-        dates=df_curnode.index
-
-        #handling "no data"
-        if len(dates)<1:
-            print curnodeID 
-            xzlist.append(pd.Series(data=[0],index=[df.index[0]]))
-            xylist.append(pd.Series(data=[0],index=[df.index[0]]))
-            #xlist.append(pd.Series(data=[seg_len],index=[df.index[0]]))
-            continue
-
-        #extracting component displacements as series
-        xz=pd.Series(data=df_curnode['xzlin'], index=df_curnode.index, name=curnodeID+1)
-        xy=pd.Series(data=df_curnode['xylin'], index=df_curnode.index, name=curnodeID+1)
-        x=pd.Series(data=df_curnode['xlin'], index=df_curnode.index, name=curnodeID+1)
-        
-        #resampling series to 30-minute intervals
-        xz=xz.resample('30Min',how='mean',base=0)
-        xy=xy.resample('30Min',how='mean',base=0)
-        x=x.resample('30Min',how='mean',base=0)
-        
-        #appending resampled series to list
-        xzlist.append(xz)
-        xylist.append(xy)
-        xlist.append(x)
-
-    #creating unfilled XZ, XY and X dataframes
-    xzdf=pd.concat([xzlist[num_nodes-a-1] for a in range(num_nodes)] ,axis=1,join='outer', names=[num_nodes-b for b in range(num_nodes)])
-    xydf=pd.concat([xylist[num_nodes-a-1] for a in range(num_nodes)] ,axis=1,join='outer', names=[num_nodes-b for b in range(num_nodes)])
-    xdf=pd.concat([xlist[num_nodes-a-1] for a in range(num_nodes)] ,axis=1,join='outer', names=[num_nodes-b for b in range(num_nodes)])  
-    return xzdf, xydf, xdf
-
 
 
 
@@ -86,17 +24,42 @@ def nodes_to_columns(df, num_nodes):
     
 
 ##set/get values from config file
+
+#time interval between data points, in hours
+data_dt=0.5
+
+#length of real-time monitoring window, in days
 rt_window_length=3.
-roll_window_size=7
+
+#length of rolling/moving window operations in hours
+roll_window_length=3.
+
+#number of rolling window operations in the whole monitoring analysis
 num_roll_window_ops=2
+
+#INPUT/OUTPUT FILES
+
+#local file paths
 columnproperties_path='/home/dynaslope-l5a/SVN/Dynaslope/updews-pycodes/Stable Versions/'
-columnproperties_file='column_properties.csv'
-columnproperties_headers=['colname','num_nodes','seg_len']
 purged_path='/home/dynaslope-l5a/Dropbox/Senslope Data/Purged/New/'
-purged_file='_.csv'
 monitoring_path='/home/dynaslope-l5a/Dropbox/Senslope Data/Purged/Monitoring/'
+LastGoodData_path='/home/dynaslope-l5a/Dropbox/Senslope Data/Purged/LastGoodData/'
+proc_monitoring_path='/home/dynaslope-l5a/Dropbox/Senslope Data/Proc2/Monitoring/'
+
+#file names
+columnproperties_file='column_properties.csv'
+purged_file='.csv'
 monitoring_file='.csv'
-monitoring_file_headers=['ts','nID','xa', 'ya', 'za', 'm']
+LastGoodData_file='.csv'
+proc_monitoring_file='.csv'
+
+#file headers
+columnproperties_headers=['colname','num_nodes','seg_len']
+purged_file_headers=['ts','id','x', 'y', 'z', 'm']
+monitoring_file_headers=['ts','id','x', 'y', 'z', 'm']
+LastGoodData_file_headers=['ts','id','x', 'y', 'z', 'm']
+proc_monitoring_file_headers=['ts','id','x', 'y', 'z', 'm']
+
 
 
 
@@ -108,38 +71,68 @@ monitoring_file_headers=['ts','nID','xa', 'ya', 'za', 'm']
 
 
 #MAIN
-end, start, offsetstart=gf.get_rt_window(rt_window_length,roll_window_size,num_roll_window_ops)
-print end, start, offsetstart
+
+#1. setting date boundaries for real-time monitoring window
+roll_window_numpts=int(1+roll_window_length/data_dt)
+end, start, offsetstart=gf.get_rt_window(rt_window_length,roll_window_numpts,num_roll_window_ops)
+
+#2. getting all column properties
 sensors=pd.read_csv(columnproperties_path+columnproperties_file,names=columnproperties_headers,index_col=None)
 
-
+print "Generating PROC monitoring data for:"
 for s in range(len(sensors)):
     
-    #getting current column properties
+    
+    #3. getting current column properties
     colname,num_nodes,seg_len=sensors['colname'][s],sensors['num_nodes'][s],sensors['seg_len'][s]
     
-    print "\nDATA for ",colname," as of ", end.strftime("%Y-%m-%d %H:%M")
+##    print "\nDATA for ",colname," as of ", end.strftime("%Y-%m-%d %H:%M")
 
-    #importing monitoring csv file of current column to dataframe
-    monitoring=pd.read_csv(monitoring_path+colname+monitoring_file,names=monitoring_file_headers,parse_dates=[0],index_col=[0])
+    try:
+        #4. importing monitoring csv file of current column to dataframe
+        monitoring=pd.read_csv(monitoring_path+colname+monitoring_file,names=monitoring_file_headers,parse_dates=[0],index_col=[1])
+
+        #5. extracting data within monitoring window
+        monitoring=monitoring[(monitoring.ts>=offsetstart)&(monitoring.ts<=end)]
+        
+        #6. importing LastGoodData csv file of current column to dataframe
+        LastGoodData=pd.read_csv(LastGoodData_path+colname+LastGoodData_file,names=LastGoodData_file_headers,parse_dates=[0],index_col=[1])
+
+        #7. extracting last data outside monitoring window
+        LastGoodData=LastGoodData[(LastGoodData.ts<offsetstart)]
+
+        #8. appending LastGoodData to monitoring
+        monitoring=monitoring.append(LastGoodData)
+        
+        #9. replacing date of data outside monitoring window with first date of monitoring window
+        monitoring.loc[monitoring.ts < offsetstart, ['ts']] = offsetstart
+
+        #10. computing corresponding horizontal linear displacements (xz,xy), and appending as columns to dataframe
+        monitoring['xz'],monitoring['xy']=gf.accel_to_lin_xz_xy(seg_len,monitoring.x.values,monitoring.y.values,monitoring.z.values)
+        
+        #11. removing unnecessary columns x,y,z
+        monitoring=monitoring.drop(['x','y','z'],axis=1)
+
+        #12. setting ts as index
+        monitoring['id']=monitoring.index.values
+        monitoring=monitoring.set_index('ts')
+
+        #13. reordering columns
+        monitoring=monitoring[['id','xz','xy','m']]
+
+        
+    ##    print "\n",colname
+    ##    print monitoring.tail(20)
+
+        monitoring.to_csv(proc_monitoring_path+colname+proc_monitoring_file,sep=',', header=False,mode='w')
+
+        print "     ",colname
+
+    except:
+        #ERROR ENCOUNTERED
+        print "     ",colname, "...FAILED"
     
-    print monitoring
-    break
-
-    #extracting last data per node
-    last_data=pd.DataFrame(data=None)
-    for n in range(1,1+num_nodes):
-        last_data=last_data.append(purged[(purged.index.values==n)].tail(1), ignore_index=False)
-    print last_data
-
     
-    #getting dataframe rows within real-time window
-    purged=purged[(purged['ts']>=offsetstart)]
-
-    
-    print purged
-
-    break
     
     
 
