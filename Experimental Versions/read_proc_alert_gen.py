97138530ee857e01d18c69ae3af39ea1306415dc
ricsatjr@gmail.com <ricsatjr@gmail.com@69feadb2-5f6b-d0c9-4c76-7ef1b510d1ee>
2014-07-09 06:37:16 +0000

    - added output for zeroed and offset tilt and velocity values

--- Experimental Versions/read_proc_alert_gen.py
+++ Experimental Versions/read_proc_alert_gen.py
@@ -86,7 +86,8 @@ def create_fill_smooth_df(series_list,num_nodes,monwin, roll_window_numpts, to_f
         #smoothing dataframes with moving average
         df=pd.rolling_mean(df,window=roll_window_numpts)[roll_window_numpts-1:]
 
-    return df
+    #returning rounded-off values within monitoring window
+    return np.round(df[(df.index>=monwin.index[0])&(df.index<=monwin.index[-1])],4)
 
 def compute_col_pos(xz,xy,col_pos_end, col_pos_interval, col_pos_number):
 
@@ -129,7 +130,8 @@ def compute_col_pos(xz,xy,col_pos_end, col_pos_interval, col_pos_number):
     cs_xz[num_nodes+1]=0
     cs_xy[num_nodes+1]=0
 
-    return cs_x, cs_xz, cs_xy
+    
+    return np.round(cs_x,4), np.round(cs_xz,4), np.round(cs_xy,4)
     
 def compute_node_inst_vel(xz,xy,roll_window_numpts): 
     #setting up time units in days
@@ -154,19 +156,55 @@ def compute_node_inst_vel(xz,xy,roll_window_numpts):
             vel_xz[n]=np.zeros(len(vel_xz.index))
             vel_xy[n]=np.zeros(len(vel_xy.index))
 
-    return vel_xz, vel_xy
+    #returning rounded-off values
+    return np.round(vel_xz,4), np.round(vel_xy,4)
 
-def process_df_to_out(xz,xy,vel_xz,vel_xy,cs_x,cs_xz,cs_xy,start,proc_monitoring_path,colname,proc_monitoring_file):
-    df_list=[xz,xy,vel_xz,vel_xy,cs_x,cs_xz,cs_xy]
-    df_name_list=['xz','xy','xz_vel','xy_vel','x_cs','xz_cs','xy_cs']
+def df_to_out(colname,xz,xy,
+              vel_xz,vel_xy,
+              cs_x,cs_xz,cs_xy,
+              proc_monitoring_path,
+              proc_monitoring_file):
+
+    monwindate=(xz.index>=vel_xz.index[0])&(xz.index<=vel_xz.index[-1])
+
+    #resizing dataframes
+    xz=xz[(xz.index>=vel_xz.index[0])&(xz.index<=vel_xz.index[-1])]
+    xy=xy[(xy.index>=vel_xz.index[0])&(xy.index<=vel_xz.index[-1])]
+    cs_x=cs_x[(cs_x.index>=vel_xz.index[0])&(cs_x.index<=vel_xz.index[-1])]
+    cs_xz=cs_xz[(cs_xz.index>=vel_xz.index[0])&(cs_xz.index<=vel_xz.index[-1])]
+    cs_xy=cs_xy[(cs_xy.index>=vel_xz.index[0])&(cs_xy.index<=vel_xz.index[-1])]
+
+    #creating zeroed and offset dataframes
+    xz_0off=df_add_offset_col(df_zero_initial_row(xz),0.15)
+    xy_0off=df_add_offset_col(df_zero_initial_row(xy),0.15)
+    vel_xz_0off=df_add_offset_col(df_zero_initial_row(vel_xz),0.015)
+    vel_xy_0off=df_add_offset_col(df_zero_initial_row(vel_xy),0.015)
+    cs_xz_0=df_zero_initial_row(cs_xz)
+    cs_xy_0=df_zero_initial_row(cs_xy)
+
+    #writing to csv
+    df_list=np.asarray([[xz,'xz'],
+             [xy,'xy'],
+             [xz_0off,'xz_0off'],
+             [xy_0off,'xy_0off'],
+             [vel_xz,'xz_vel'],
+             [vel_xy,'xy_vel'],
+             [vel_xz_0off,'xz_vel_0off'],
+             [vel_xy_0off,'xy_vel_0off'],
+             [cs_x,'x_cs'],
+             [cs_xz,'xz_cs'],
+             [cs_xy,'xy_cs'],
+             [cs_xz_0,'xz_cs_0'],
+             [cs_xy_0,'xy_cs_0']])
+
+    
     for d in range(len(df_list)):
-        #resizing and rounding dataframes
-        df_list[d]=np.round(df_list[d],4)[(df_list[d].index>=start)]
-        #writing to csv
-        df=df_list[d]
-        df.to_csv(proc_monitoring_path+"/csv_to_plot/"+colname+"_"+df_name_list[d]+proc_monitoring_file,
+        df=df_list[d,0]
+        fname=df_list[d,1]
+        df.to_csv(proc_monitoring_path+'csv_to_plot/'+colname+" "+fname+proc_monitoring_file,
                   sep=',', header=False,mode='w')
-    return df_list
+
+    return xz,xy,   xz_0off,xy_0off,   vel_xz,vel_xy, vel_xz_0off, vel_xy_0off, cs_x,cs_xz,cs_xy,   cs_xz_0,cs_xy_0
 
 def alert_generation(colname,xz,xy,vel_xz,vel_xy,num_nodes, T_disp, T_velA1, T_velA2, k_ac_ax,
                      num_nodes_to_check,end,proc_monitoring_path,proc_monitoring_file):
@@ -189,7 +227,7 @@ def alert_generation(colname,xz,xy,vel_xz,vel_xy,num_nodes, T_disp, T_velA1, T_v
 
     return alert_out
     
-def plot_column_positions(colname,cs_x,cs_xz,cs_xy):
+def plot_column_positions(colname,x,xz,xy):
     try:
         fig=plt.figure(1)
         plt.clf()
@@ -198,22 +236,22 @@ def plot_column_positions(colname,cs_x,cs_xz,cs_xy):
         ax_xy=fig.add_subplot(122,sharex=ax_xz,sharey=ax_xz)
 
         for i in cs_x.index:
-            curcolpos_x=cs_x[(cs_x.index==i)].values
+            curcolpos_x=x[(x.index==i)].values
 
             curax=ax_xz
-            curcolpos_xz=cs_xz[(cs_xz.index==i)].values
+            curcolpos_xz=xz[(xz.index==i)].values
             curax.plot(curcolpos_xz[0],curcolpos_x[0],'.-')
             curax.set_xlabel('xz')
             curax.set_ylabel('x')
 
             curax=ax_xy
-            curcolpos_xy=cs_xy[(cs_xy.index==i)].values
+            curcolpos_xy=xy[(xy.index==i)].values
             curax.plot(curcolpos_xy[0],curcolpos_x[0],'.-', label=i)
             curax.set_xlabel('xy')
 
         fig.tight_layout()
         plt.legend(fontsize='x-small')
-        plt.show()
+        
     
     except:        
         print colname, "ERROR in plotting column position"
@@ -223,45 +261,56 @@ def plot_disp_vel(colname, xz,xy,xz_vel,xy_vel):
     try:
         fig=plt.figure(2)
         plt.clf()
-        ax_xzd=fig.add_subplot(221)
-        ax_xyd=fig.add_subplot(222,sharex=ax_xzd,sharey=ax_xzd)
+        ax_xzd=fig.add_subplot(141)
+        ax_xyd=fig.add_subplot(142,sharex=ax_xzd,sharey=ax_xzd)
 
-        ax_xzv=fig.add_subplot(223,sharex=ax_xzd)
-        ax_xyv=fig.add_subplot(224,sharex=ax_xzv,sharey=ax_xzv)
+        ax_xzv=fig.add_subplot(143,sharex=ax_xzd)
+        ax_xyv=fig.add_subplot(144,sharex=ax_xzv,sharey=ax_xzv)
 
         curax=ax_xzd
         plt.sca(curax)
-        zero_plot(xz,curax)
+        xz.plot(ax=curax,legend=False)
         curax.set_title(colname+' XZ')
         curax.set_ylabel('disp, m', fontsize='small')
-        curax.set_ylim(-0.1,0.1)
+        #curax.set_ylim(-0.1,0.1)
         
         curax=ax_xyd
         plt.sca(curax)
-        zero_plot(xy,curax)
+        xy.plot(ax=curax,legend=False)
         curax.set_title(colname+' XY')
         
         curax=ax_xzv
         plt.sca(curax)
-        zero_plot(vel_xz,curax)
+        xz_vel.plot(ax=curax,legend=False)
         curax.set_ylabel('vel, m/day', fontsize='small')
-        curax.set_ylim(-0.01,0.01)
+        #curax.set_ylim(-0.01,0.01)
         
         curax=ax_xyv
         plt.sca(curax)
-        zero_plot(vel_xy,curax)
+        xy_vel.plot(ax=curax,legend=False)
         
         fig.tight_layout()
-        plt.show()
+        
         
     except:      
         print colname, "ERROR in plotting displacements and velocities"
     return
 
 
-def zero_plot(df,ax):
-    df0 = df-df.loc[(df.index==df.index[0])].values.squeeze()
-    df0.plot(ax=ax,legend=False)
+def df_zero_initial_row(df):
+    #zeroing time series to initial value;
+    #essentially, this subtracts the value of the first row
+    #from all the rows of the dataframe
+    return np.round(df-df.loc[(df.index==df.index[0])].values.squeeze(),4)
+
+def df_add_offset_col(df,offset):
+    #adding offset value based on column value (node ID);
+    #topmost node (node 1) has largest offset
+    for n in range(1,1+len(df.columns)):
+        df[n]=df[n] + (len(df.columns)-n)*offset
+    return np.round(df,4)
+        
+    
     
 
 
@@ -364,25 +413,25 @@ for s in range(len(sensors)):
     cs_x, cs_xz, cs_xy=compute_col_pos(xz,xy,monwin.index[-1], col_pos_interval, col_pos_num)
     
     #9. processing dataframes for output
-    df_list=process_df_to_out(xz,xy,vel_xz,vel_xy,cs_x,cs_xz,cs_xy,start,proc_monitoring_path,colname,proc_monitoring_file)
-    xz=df_list[0]
-    xy=df_list[1]
-    vel_xz=df_list[2]
-    vel_xy=df_list[3]
-    cs_x=df_list[4]
-    cs_xz=df_list[5]
-    cs_xy=df_list[6]
-
+    xz,xy,xz_0off,xy_0off,vel_xz,vel_xy, vel_xz_0off, vel_xy_0off,cs_x,cs_xz,cs_xy,cs_xz_0,cs_xy_0 = df_to_out(colname,xz,xy,
+                                                                                                               vel_xz,vel_xy,
+                                                                                                               cs_x,cs_xz,cs_xy,
+                                                                                                               proc_monitoring_path,
+                                                                                                               proc_monitoring_file)
+                                                                                                                       
+    
     #10. Alert generation
     alert_out=alert_generation(colname,xz,xy,vel_xz,vel_xy,num_nodes, T_disp, T_velA1, T_velA2, k_ac_ax,
                                num_nodes_to_check,end,proc_monitoring_path,proc_monitoring_file)
-    #print alert_out
+    
 
     #11. Plotting column positions
-    plot_column_positions(colname,cs_x,cs_xz,cs_xy)
+    #plot_column_positions(colname,cs_x,cs_xz_0,cs_xy_0)
 
     #12. Plotting displacement and velocity
-    plot_disp_vel(colname, xz,xy,vel_xz,vel_xy)
+    #plot_disp_vel(colname, xz_0off,xy_0off, vel_xz_0off, vel_xy_0off)
+
+    #plt.show()
 
 
     
