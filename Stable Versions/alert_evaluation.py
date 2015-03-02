62af46570fa8723e99ca553d20badaff8311f3d7
piereluya@gmail.com <piereluya@gmail.com@69feadb2-5f6b-d0c9-4c76-7ef1b510d1ee>
2015-03-02 01:39:03 +0000

    Committer: Piere alert_evaluaton.py
    - edited trending alert to accommodate monthly alerts

--- Stable Versions/alert_evaluation.py
+++ Stable Versions/alert_evaluation.py
@@ -293,25 +293,30 @@ def trending_col(alert,colname):
     latest_alert=latest_alert[['ts','id','col_alert']]
     latest_alert=latest_alert.set_index(['ts'])
 
-    proc_alert=pd.read_csv(proc_monitoring_path+colname+'/'+colname+" "+"alert"+proc_monitoring_file,
-                           header=None,parse_dates=[0],index_col=[0],usecols=[0,1,10])
-
-    proc_alert=proc_alert[(proc_alert.index>=end-timedelta(hours=3))]
-    proc_alert.append(latest_alert)
-    proc_alert[10]=proc_alert[10].map({'nd':-1,'a0':0,'a1':1,'a2':2})
-
-    mode_node=[]
-    for n in range(1,len(alert)+1):
-        trend_col=proc_alert[proc_alert[1]==n]
-        trend_col=trend_col[10].tolist()
-        if len(trend_col)==0:
-            mode_node.append(-1)
-        else:
-            trend_col=max(gf.getmode(trend_col))
-            mode_node.append(trend_col)
-
-    alert['trend_col_alert']=mode_node
-    alert['trend_col_alert']=alert['trend_col_alert'].map({-1:'nd',0:'a0',1:'a1',2:'a2'})
+    try:
+      proc_alert=pd.read_csv(proc_monitoring_path+colname+'/'+colname+" "+"alert"+proc_monitoring_file,
+                             header=None,parse_dates=[0],index_col=[0],usecols=[0,1,10])
+  
+      proc_alert=proc_alert[(proc_alert.index>=end-timedelta(hours=3))]
+      proc_alert.append(latest_alert)
+      proc_alert[10]=proc_alert[10].map({'nd':-1,'a0':0,'a1':1,'a2':2})
+  
+      mode_node=[]
+      for n in range(1,len(alert)+1):
+          trend_col=proc_alert[proc_alert[1]==n]
+          trend_col=trend_col[10].tolist()
+          if len(trend_col)==0:
+              mode_node.append(-1)
+          else:
+              trend_col=max(gf.getmode(trend_col))
+              mode_node.append(trend_col)
+  
+      alert['trend_col_alert']=mode_node
+      alert['trend_col_alert']=alert['trend_col_alert'].map({-1:'nd',0:'a0',1:'a1',2:'a2'})
+     
+     except:
+       print "No sensor data for the past 30 days"
+       alert['trend_col_alert']=-1*np.ones(len(alert))
 
     return alert
     
