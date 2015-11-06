9d4531e044107334c3082ba16d93e4a2f56431b7
updewsprado <updews.prado@gmail.com>
2015-11-06 15:23:02 +0800

    - Created functions that can get user arguments to get desired results for filtered data showing

--- Data Analysis/outputFilteredData.py
+++ Data Analysis/outputFilteredData.py
@@ -1,24 +1,82 @@
 import querySenslopeDb as qs
 import filterSensorData as fs
+import sys
 
-#targetTable = 'bolb'
-node = 9
-sensors = qs.GetSensorDF()
-#print sensors
+#Get filtered data for all sites
+def getFilteredAll():
+    try:
+        start = sys.argv[1]
+    except IndexError:
+        start = ''
+        
+    try:
+        end = sys.argv[2] 
+    except IndexError:
+        end = ''     
+        
+    try:
+        msgid = sys.argv[3] 
+    except IndexError:
+        msgid = 32
+    
+    sensors = qs.GetSensorDF()
+    
+    for s in range(len(sensors)):
+        targetTable = sensors.name[s]
+        df = qs.GetRawAccelData(siteid = targetTable, fromTime = start, toTime = end, msgid = msgid)
+        numElements = len(df.index)
+        qs.PrintOut("Number of %s Raw elements: %s" % (targetTable, numElements))
+        
+        if numElements > 0:
+            df_filtered = fs.applyFilters(df, orthof=True, rangef=True, outlierf=True)
+            numFiltered = len(df_filtered.index)
+            
+            if numFiltered > 0:
+                qs.PrintOut("Number of %s filtered elements: %s" % (targetTable, numFiltered))
+                #print df_filtered
+            else:
+                print "No valid filtered data for %s" % (targetTable)
 
-for s in range(len(sensors)):
-    targetTable = sensors.name[s]
-    df = qs.GetRawAccelData(siteid = targetTable, fromTime = "2015-10-15", toTime = "2015-10-25", msgid = 33, targetnode = node)
+#get filtered data for a selected site
+def getFilteredData():
+    try:
+        site = sys.argv[1]
+    except IndexError:
+        print "No site has been selected. Script unable to run!"
+        
+    try:
+        node = sys.argv[2] 
+    except IndexError:
+        node = '-1'   
+
+    try:
+        start = sys.argv[3]
+    except IndexError:
+        start = ''
+        
+    try:
+        end = sys.argv[4] 
+    except IndexError:
+        end = ''       
+        
+    try:
+        msgid = sys.argv[5] 
+    except IndexError:
+        msgid = 32
+
+    df = qs.GetRawAccelData(siteid = site, fromTime = start, toTime = end, msgid = msgid, targetnode = node)
     numElements = len(df.index)
-    #print "Number of %s Raw elements: %s" % (targetTable, numElements)
-    qs.PrintOut("Number of %s Raw elements: %s" % (targetTable, numElements))
+    qs.PrintOut("Number of %s Raw elements: %s" % (site, numElements))
     
     if numElements > 0:
         df_filtered = fs.applyFilters(df, orthof=True, rangef=True, outlierf=True)
         numFiltered = len(df_filtered.index)
         
         if numFiltered > 0:
-            qs.PrintOut("Number of %s filtered elements: %s" % (targetTable, numFiltered))
-            #print df_filtered
+            qs.PrintOut("Number of %s filtered elements: %s" % (site, numFiltered))
+
+            dfajson = df_filtered.to_json(orient="records",date_format='iso')
+            dfajson = dfajson.replace("T"," ").replace("Z","").replace(".000","")
+            print dfajson
         else:
-            print "No valid filtered data for %s" % (targetTable)
+            print "No valid filtered data for %s" % (site)
