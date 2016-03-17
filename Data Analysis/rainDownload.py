68f732ba3b99cca52f239b2117ddc58390e4ac7e
Meryll Viernes <meryllviernes08@gmail.com>
2016-03-17 17:35:46 +0800

    - modified: readProcAlertGenWithTrendingAlert.py, Rainfall.py, rainDownload.py, generateProcMonitoring.py; added an option to on/off plotter/output files

--- Data Analysis/rainDownload.py
+++ Data Analysis/rainDownload.py
@@ -13,30 +13,29 @@ cfg.read('IO-config.txt')
 ASTIpath = cfg.get('I/O', 'ASTIpath')
 CSVFormat = cfg.get('I/O', 'CSVFormat')
 
-## This will download CSV files that contain 14 days worth of data
-#def getrain(site, gauge_num, rain_noah, offsetstart):
-#    
-#    if not math.isnan(rain_noah):
-#        rain_noah = int(rain_noah)
-#    
-#    url = 'http://www.dewslandslide.com/ajax/dlRain2.php?site=' + str(rain_noah)
-#    print "downloading " + site + str(gauge_num)
-#    urllib.urlretrieve(url, ASTIpath + site + ".csv")
-    
+#To Output File or not
+PrintASTIdata = cfg.getboolean('I/O','PrintASTIdata')
 
-    
+## This will download CSV files that contain 13 days worth of data   
 def getrain(site, gauge_num, rain_noah, offsetstart):
-    
-    if not math.isnan(rain_noah):
-        rain_noah = int(rain_noah)
 
-    db, cur = SenslopeDBConnect(Namedb)
+    try:    
+        if not math.isnan(rain_noah):
+            rain_noah = int(rain_noah)
     
-    query = "select timestamp,rval from senslopedb.rain_noah_%s" % str(rain_noah)
-    query = query + " where timestamp >= timestamp('%s')" % offsetstart
-    query = query + " order by timestamp desc"
-    df =  GetDBDataFrame(query)
-    df.columns = ['timestamp','rain']
-    df.timestamp = pd.to_datetime(df.timestamp)
-    df.to_csv(ASTIpath + site + CSVFormat, sep = ',', mode = 'w', index = False, header = False)
-    
+        db, cur = SenslopeDBConnect(Namedb)
+        
+        query = "select timestamp,rval from senslopedb.rain_noah_%s" % str(rain_noah)
+        query = query + " where timestamp >= timestamp('%s')" % offsetstart
+        query = query + " order by timestamp desc"
+        df =  GetDBDataFrame(query)
+        df.columns = ['timestamp','rain']
+        df.timestamp = pd.to_datetime(df.timestamp)
+        if PrintASTIdata:
+            df.to_csv(ASTIpath + site + CSVFormat, sep = ',', mode = 'w', index = False, header = False)
+        df.set_index('timestamp', inplace = True)
+        return df
+    except:
+        print 'Table senslopedb.rain_noah_' + str(rain_noah) + " doesn't exist"
+        df = pd.DataFrame(data=None)
+        return df
