d0d815342d2e5f5a4fc6ea699df9964857da9fc9
Upitdc Prado <upitdc.prado@gmail.com>
2016-03-21 15:37:43 +0800

    + testAccelTemplate.py, Added Acceleromter Filter Test Template - testSoms.py, Added path for accessing querySenslopedb.py

--- Data Analysis/Accel/testAccelTemplate.py
+++ Data Analysis/Accel/testAccelTemplate.py
@@ -9,7 +9,9 @@ import os
 import sys
 import time
 import pandas as pd
-import ConvertSomsRaw as soms
+
+#TODO: Add the accelerometer filter module you need to test
+#import newAccelFilter as naf
 
 #include the path of "Data Analysis" folder for the python scripts searching
 path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
@@ -27,18 +29,12 @@ import querySenslopeDb as qs
 fdate = "2015-01-01 00:00:00"
 tdate = time.strftime("%Y-%m-%d %H:%M")
 
-#SOMS Inputs should be:
-#   a. column
-#   b. nid
-#   c. start date
-#   d. end date
-
 try:
     db, cur = qs.SenslopeDBConnect('senslopedb')
     print '>> Connected to database'
 
-    #Get all column names with SOMS
-    queryColumns = 'SHOW TABLES LIKE "%m"'
+    #Get all column names with installation status of "Installed"
+    queryColumns = 'SELECT name, version FROM site_column WHERE installation_status = "Installed" ORDER BY s_id ASC'
     try:
         cur.execute(queryColumns)
     except:
@@ -55,20 +51,24 @@ try:
             cur.execute(queryNodes)
             
             nodes = cur.fetchall()
-            print nodes
         
             for nodeData in nodes:
                 node = nodeData[0]
                 print "%s: %s" % (columnName, node)
-                
-                test = soms.getsomsrawdata(columnName, node, fdate, tdate)
-                #print test
-                print "row count: %s" % (len(test.index))
+                   
+#                Accel Inputs should be:
+#                   a. column
+#                   b. nid
+#                   c. version
+#                   d. start date
+#                   e. end date
+                   
+#                TODO: add your accelerometer filter here 
+#                test = naf.newAccelFilterFxn(columnName, node, version, fdate, tdate)
+#                print test
+#                print "row count: %s" % (len(test.index))                
                 
                 pass
 
 except IndexError:
     print '>> Error in writing extracting database data to files..'
-
-#test = soms.getsomsrawdata(column, gid, fdate, tdate)
-#print test
