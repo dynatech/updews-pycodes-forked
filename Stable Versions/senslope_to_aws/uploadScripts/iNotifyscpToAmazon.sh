16efbf14fd1a2c2705aa92871ce9e54a0797ddb8
Upitdc Prado <upitdc.prado@gmail.com>
2016-06-09 15:29:04 +0800

    + Added iNotifyscpToAmazon.sh, a real-time script uploader for reduced latency in data reflection from local server to website

---
+++ Stable Versions/senslope_to_aws/uploadScripts/iNotifyscpToAmazon.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+cd ~/Desktop/websiteUploads
+
+inotifywait -q -m -e close_write ~/Desktop/websiteUploads |
+while read folder events filename; do
+#    $target = $filename | awk '{ print $(NF) }'
+#    if [ "$filename" = *.sql ]; then
+    if expr index "$filename" ".sql"; then
+        #echo "$filename is a sql file"
+        scp -i /keyPair/senslopeInstance.pem $filename ubuntu@www.dewslandslide.com:~/sqldumps
+
+        if [ "$?" -eq 0 ]; then
+            echo "Successfully uploaded $filename"
+            rm $filename
+        else
+            echo "Failed to upload $filename"
+        fi
+    else
+        echo "$filename is a file that won't be uploaded"
+    fi
+
+#    echo "$filename was written"
+
+#    echo "new event"
+done
