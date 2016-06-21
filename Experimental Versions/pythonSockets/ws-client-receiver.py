1ad2aba101556b40d90fb534f69c5727a3c1e3e4
Upitdc Prado <upitdc.prado@gmail.com>
2016-06-21 16:01:31 +0800

    - Added classes "DewsClientGSMProtocol" and "DewsClientRegularProtocol" to the "Web Sockets" section of the dewsSocketLib.py - Cleaned code for "ws-client-receiver.py"

--- Experimental Versions/pythonSockets/ws-client-receiver.py
+++ Experimental Versions/pythonSockets/ws-client-receiver.py
@@ -1,59 +1,16 @@
 import sys
 import json
 import dewsSocketLib as dsl
-import queryPiDb as qpi
 
 from twisted.python import log
 from twisted.internet import reactor
 from autobahn.twisted.websocket import WebSocketClientProtocol, \
     WebSocketClientFactory
 
-class DewsClientProtocol(WebSocketClientProtocol):
-
-    def onConnect(self, response):
-        print("Server connected: {0}".format(response.peer))
-
-    def onOpen(self):
-        print("WebSocket connection open.")
-
-        # def hello():
-        #     self.sendMessage(u"Hello, world!".encode('utf8'))
-        #     # self.sendMessage(b"\x00\x01\x03\x04", isBinary=True)
-        #     self.factory.reactor.callLater(1, hello)
-
-        # start sending messages every second ..
-        # hello()
-
-    def onMessage(self, payload, isBinary):
-        if isBinary:
-            print("Binary message received: {0} bytes".format(len(payload)))
-        else:
-            msg = format(payload.decode('utf8'))
-            print("Text message received: %s" % msg)
-
-            #The local ubuntu server is expected to receive a JSON message
-            #parse the numbers from the message
-            try:
-                parsed_json = json.loads(msg)
-                
-                recipients = parsed_json['numbers']
-                print "Recipients of Message: %s" % (len(recipients))
-                
-                for recipient in recipients:
-                    print recipient
-                
-                message = parsed_json['msg']
-                
-                dsl.sendMessageToGSM(recipients, message)
-            except:
-                print "Error: Please check the JSON construction of your message"
-
-    def onClose(self, wasClean, code, reason):
-        print("WebSocket connection closed: {0}".format(reason))
-
-def mainFunc(host, port):
+def wsClientReceiver(host, port):
     factory = WebSocketClientFactory(u"ws://%s:%s" % (host,port))
-    factory.protocol = DewsClientProtocol
+    # factory.protocol = DewsClientProtocol
+    factory.protocol = dsl.DewsClientGSMProtocol
 
     reactor.connectTCP(host, port, factory)
     reactor.run()
@@ -64,4 +21,4 @@ if __name__ == '__main__':
     host = "www.codesword.com"
     port = 5050
 
-    mainFunc(host, port)
+    wsClientReceiver(host, port)
