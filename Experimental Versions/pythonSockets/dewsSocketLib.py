e4f050b7dfad34b547701f8bc41f86d494f0d52a
Upitdc Prado <upitdc.prado@gmail.com>
2016-06-24 11:58:48 +0800

    + dewsSocketLeanLib.py, segregated the simpler functions for Raspberry Pi porting purposes - dewsSocketLib.py, imports dewsSocketLeanLib.py for the simpler/core functions

--- Experimental Versions/pythonSockets/dewsSocketLib.py
+++ Experimental Versions/pythonSockets/dewsSocketLib.py
@@ -12,18 +12,21 @@ import sys
 import time
 import json
 import pandas as pd
-#import datetime
-from datetime import datetime
-import queryPiDb as qpi
+# #import datetime
+# from datetime import datetime
+# import queryPiDb as qpi
 
-#Simple Python WebSocket
-from websocket import create_connection
+# #Simple Python WebSocket
+# from websocket import create_connection
 
 #Autobahn and Twisted
 from twisted.python import log
 from twisted.internet import reactor
 from autobahn.twisted.websocket import WebSocketClientProtocol, \
     WebSocketClientFactory
+
+import dewsSocketLeanLib as dsll
+
 #import MySQLdb
 
 #TODO: Add the accelerometer filter module you need to test
@@ -37,127 +40,126 @@ del path
 
 import querySenslopeDb as qs
 
-###############################################################################
-# GSM Functionalities
-###############################################################################
+# ###############################################################################
+# # GSM Functionalities
+# ###############################################################################
 
-def writeToSMSoutbox(number, msg):
-    try:
-        qInsert = """INSERT INTO smsoutbox (recepients,sms_msg,send_status)
-                    VALUES ('%s','%s','UNSENT');""" % (number,msg)
-        print qInsert
-        qpi.ExecuteQuery(qInsert)        
+# def writeToSMSoutbox(number, msg):
+#     try:
+#         qInsert = """INSERT INTO smsoutbox (recepients,sms_msg,send_status)
+#                     VALUES ('%s','%s','UNSENT');""" % (number,msg)
+#         print qInsert
+#         qpi.ExecuteQuery(qInsert)        
     
-    except IndexError:
-        print '>> Error in writing extracting database data to files..'
+#     except IndexError:
+#         print '>> Error in writing extracting database data to files..'
 
-def sendMessageToGSM(recipients, msg):
-    db, cur = qpi.SenslopeDBConnect('senslopedb')
-    print '>> Connected to database'
+# def sendMessageToGSM(recipients, msg):
+#     db, cur = qpi.SenslopeDBConnect('senslopedb')
+#     print '>> Connected to database'
     
-    for number in recipients:
-        print "%s: %s" % (number, msg)
-        writeToSMSoutbox(number, msg)
+#     for number in recipients:
+#         print "%s: %s" % (number, msg)
+#         writeToSMSoutbox(number, msg)
 
-def sendTimestampToGSM(host, port, recipients):
-    db, cur = qpi.SenslopeDBConnect('senslopedb')
-    print '>> Connected to database'
+# def sendTimestampToGSM(host, port, recipients):
+#     db, cur = qpi.SenslopeDBConnect('senslopedb')
+#     print '>> Connected to database'
     
-    i = datetime.now()
-    txtmsg = "%s: Test text message from RPi" % (i)
+#     i = datetime.now()
+#     txtmsg = "%s: Test text message from RPi" % (i)
     
-    for number in recipients:
-        sendDataFullCycle(host, port, txtmsg)
-        print "%s: %s" % (number, txtmsg)
-        writeToSMSoutbox(number, txtmsg)
+#     for number in recipients:
+#         sendDataFullCycle(host, port, txtmsg)
+#         print "%s: %s" % (number, txtmsg)
+#         writeToSMSoutbox(number, txtmsg)
 
-###############################################################################
-# Regular Sockets
-###############################################################################
+# ###############################################################################
+# # Regular Sockets
+# ###############################################################################
 
-#Open a socket connection
-def openSocketConn(host, port):
-    s = socket.socket()
-    s.connect((host, port))
-    return s
+# #Open a socket connection
+# def openSocketConn(host, port):
+#     s = socket.socket()
+#     s.connect((host, port))
+#     return s
 
-#Close a socket connection
-def closeSocketConn(sock_conn):
-    sock_conn.close()
+# #Close a socket connection
+# def closeSocketConn(sock_conn):
+#     sock_conn.close()
 
-#One full cycle of opening connection
-# sending data and closing connection
-def sendDataFullCycle(host, port, msg):
-    s = socket.socket()    
-    s.connect((host, port))
-    print msg
-    s.send(msg)
-    s.close()
+# #One full cycle of opening connection
+# # sending data and closing connection
+# def sendDataFullCycle(host, port, msg):
+#     s = socket.socket()    
+#     s.connect((host, port))
+#     print msg
+#     s.send(msg)
+#     s.close()
 
-def sendColumnNamesToSocket(host, port):
-    try:
-        db, cur = qs.SenslopeDBConnect('senslopedb')
-        print '>> Connected to database'
+# def sendColumnNamesToSocket(host, port):
+#     try:
+#         db, cur = qs.SenslopeDBConnect('senslopedb')
+#         print '>> Connected to database'
     
-        #Get all column names with installation status of "Installed"
-        queryColumns = 'SELECT name, version FROM site_column WHERE installation_status = "Installed" ORDER BY s_id ASC'
-        try:
-            cur.execute(queryColumns)
-        except:
-            print '>> Error parsing database'
+#         #Get all column names with installation status of "Installed"
+#         queryColumns = 'SELECT name, version FROM site_column WHERE installation_status = "Installed" ORDER BY s_id ASC'
+#         try:
+#             cur.execute(queryColumns)
+#         except:
+#             print '>> Error parsing database'
         
-        columns = cur.fetchall()
-    #    print columns
+#         columns = cur.fetchall()
+#     #    print columns
     
-        for column in columns:
-            columnName = column[0]
-            sendDataFullCycle(host, port, columnName)
-            print columnName
+#         for column in columns:
+#             columnName = column[0]
+#             sendDataFullCycle(host, port, columnName)
+#             print columnName
     
-    except IndexError:
-        print '>> Error in writing extracting database data to files..'
+#     except IndexError:
+#         print '>> Error in writing extracting database data to files..'
 
-###############################################################################
-# Web Sockets
-###############################################################################
+# ###############################################################################
+# # Web Sockets
+# ###############################################################################
 
-#One full cycle of opening connection
-# sending data and closing connection
-def sendDataToWSS(host, port, msg):
-    try:
-        ws = create_connection("ws://%s:%s" % (host, port))
-#        print "Opened WebSocket"
-#        print msg
-        ws.send(msg)
-        print "Sent %s" % (msg)
-        ws.close()
-#        print "Closed WebSocket"
+# #One full cycle of opening connection
+# # sending data and closing connection
+# def sendDataToWSS(host, port, msg):
+#     try:
+#         ws = create_connection("ws://%s:%s" % (host, port))
+# #        print "Opened WebSocket"
+# #        print msg
+#         ws.send(msg)
+#         print "Sent %s" % (msg)
+#         ws.close()
+# #        print "Closed WebSocket"
         
-        #returns 0 on successful sending of data
-        return 0
-    except:
-        print "Failed to send data. Please check your internet connection"        
+#         #returns 0 on successful sending of data
+#         return 0
+#     except:
+#         print "Failed to send data. Please check your internet connection"        
         
-        #returns -1 on failure to send data
-        return -1
+#         #returns -1 on failure to send data
+#         return -1
     
-#No filtering yet for special characters
-def formatReceivedGSMtext(smsid, timestamp, sender, message):
-    jsonText = """{"type":"smsrcv","sms_id":"%s","timestamp":"%s","sender":["%s"],"msg":"%s"}""" % (smsid, timestamp, sender, message)
-    return jsonText    
+# #No filtering yet for special characters
+# def formatReceivedGSMtext(smsid, timestamp, sender, message):
+#     jsonText = """{"type":"smsrcv","sms_id":"%s","timestamp":"%s","sender":"%s","msg":"%s"}""" % (smsid, timestamp, sender, message)
+#     return jsonText    
     
-def sendDataToDEWS(msg):
-#    host = "www.dewslandslide.com"
-    host = "www.codesword.com"
-    port = 5050
-    sendDataToWSS(host, port, msg)
+# def sendDataToDEWS(msg):
+# #    host = "www.dewslandslide.com"
+#     host = "www.codesword.com"
+#     port = 5050
+#     sendDataToWSS(host, port, msg)
     
-def sendReceivedGSMtoDEWS(smsid, timestamp, sender, message):
-    jsonText = formatReceivedGSMtext(smsid, timestamp, sender, message)
-    sendDataToDEWS(jsonText)
+# def sendReceivedGSMtoDEWS(smsid, timestamp, sender, message):
+#     jsonText = formatReceivedGSMtext(smsid, timestamp, sender, message)
+#     sendDataToDEWS(jsonText)
 
 class DewsClientGSMProtocol(WebSocketClientProtocol):
-
     def onConnect(self, response):
         print("Server connected: {0}".format(response.peer))
 
@@ -200,7 +202,6 @@ class DewsClientGSMProtocol(WebSocketClientProtocol):
         print("WebSocket connection closed: {0}".format(reason))
 
 class DewsClientRegularProtocol(WebSocketClientProtocol):
-
     def onConnect(self, response):
         print("Server connected: {0}".format(response.peer))
 
--- Experimental Versions/pythonSockets/ws-client-remote2.py
+++ Experimental Versions/pythonSockets/ws-client-remote2.py
@@ -5,8 +5,8 @@ Created on Wed Jun 22 13:59:18 2016
 @author: PradoArturo
 """
 
-import dewsSocketLib as dsl
+import dewsSocketLeanLib as dsll
 
-msg = "~`!@#$%^&*()_-+="
+msg = "~`!@#$%^&*()_-+=qwertyuiop[]asdfghjkl;"
 
-dsl.sendReceivedGSMtoDEWS(1234, "2016-06-22 17:00:00", "09163677476", msg)
+dsll.sendReceivedGSMtoDEWS(1239, "2016-06-22 17:00:00", "09163677476", msg)
