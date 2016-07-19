153e1962cbcb68d75daec44b1cdb90de68b53655
Dynaslope Test Server <senslopenotification@gmail.com>
2016-07-19 19:13:56 +0800

    -audioalert.py

--- Data Analysis/IntegratedAlert/audioalert.py
+++ Data Analysis/IntegratedAlert/audioalert.py
@@ -1,8 +1,8 @@
+import subprocess
 import ConfigParser
 import os
-import pyglet
-pyglet.options['debug_gl'] = False
-from datetime import datetime, timedelta
+import time
+import signal
 
 output_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../..'))
 
@@ -17,17 +17,7 @@ gsm_alert = cfg.get('I/O','gsmalert')
 output_file_path = output_path + cfg.get('I/O','OutputFilePath')
 
 if os.stat(output_file_path+gsm_alert).st_size != 0:
-    start = datetime.now()
-    audio_source = pyglet.media.load('alert.wav', streaming=False)
-    looper = pyglet.media.SourceGroup(audio_source.audio_format, None)
-    looper.loop = False
-    looper.queue(audio_source)
-    player = pyglet.media.Player()
-    player.queue(looper)
-    player.play()
-    f = open(output_file_path+gsm_alert)
-    text = f.read()
-    f.close()
-    while (datetime.now() - start) <= (timedelta(hours = 0.005)):
-        print text
-    
+    cmd = 'mplayer alert.wav'
+    audioalert = subprocess.Popen(cmd, shell=True)
+    time.sleep(5)
+    os.kill(audioalert.pid, signal.SIGINT)
