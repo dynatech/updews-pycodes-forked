39b9e6dc2b3648a76665179a83712918ea62e4c0
Meryll Viernes <meryllviernes08@gmail.com>
2016-09-01 13:01:04 +0800

    - Analysis/PublicAlert.py; retriggerTS

--- Analysis/PublicAlert.py
+++ Analysis/PublicAlert.py
@@ -555,7 +555,7 @@ def main():
     PublicAlert.to_csv('PublicAlert.txt', header=True, index=None, sep='\t', mode='w')
     
     dfjson = PublicAlert.to_json(orient="records", date_format="iso")
-    dfjson = dfjson.replace('T', ' ').replace('.000Z', '')
+    dfjson = dfjson.replace('T', ' ').replace('.000Z', '').replace('retrigger S','retriggerTS')
     with open('PublicAlert.json', 'w') as w:
         w.write(dfjson)
             
