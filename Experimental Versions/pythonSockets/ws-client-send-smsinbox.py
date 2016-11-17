90385fd597218e2c67e8414ada42fd11a72f9117
Upitdc Prado <upitdc.prado@gmail.com>
2016-11-17 10:58:00 +0800

    !quick fix: use "54.166.60.233" to distribute system load

--- Experimental Versions/pythonSockets/ws-client-send-smsinbox.py
+++ Experimental Versions/pythonSockets/ws-client-send-smsinbox.py
@@ -8,7 +8,8 @@ Created on Wed Sep 23 13:59:18 2016
 import dewsSocketLeanLib as dsll
 
 port = 5050
-host = "www.dewslandslide.com"
+# host = "www.dewslandslide.com"
+url = "54.166.60.233"
 
 # Send 100 messages from smsinbox to the websocket server
 dsll.sendBatchReceivedGSMtoDEWS(host, port, 100)
