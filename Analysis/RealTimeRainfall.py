e893f24db6309ee6b3414fab9092d41ee4106c6d
Meryll Viernes <meryllviernes08@gmail.com>
2017-01-10 09:52:31 +0800

    - RealTimeRainfall.py

--- Analysis/RealTimeRainfall.py
+++ Analysis/RealTimeRainfall.py
@@ -19,38 +19,43 @@ def main(ts=datetime.now(), site=''):
     if site == '':
         while True:
             site = raw_input('site (e.g. agb): ').lower()
-            rain_props = rain_props[rain_props.name == site]
-            if len(rain_props) != 0:
+            site_rain_props = rain_props[rain_props.name == site]
+            if len(site_rain_props) != 0:
                 break
             print 'site not in the list'
     else:
-        rain_props = rain_props[rain_props.name == site]
+        site_rain_props = rain_props[rain_props.name == site]
     # 3-day threshold
-    twoyrmax = rain_props['max_rain_2year'].values[0]
+    twoyrmax = site_rain_props['max_rain_2year'].values[0]
     # 1-day threshold
     halfmax = twoyrmax/2.
-    gauge_ids = [rain_props['RG1'].values[0]] +[rain_props['RG2'].values[0]] +[rain_props['RG3'].values[0]]
+    gauge_ids = [site_rain_props['RG1'].values[0]] +[site_rain_props['RG2'].values[0]] +[site_rain_props['RG3'].values[0]]
     try:
-        if rain_props['rain_senslope'].values[0] != None:
-            gauge_ids += [rain_props['rain_senslope'].values[0]]
+        if site_rain_props['rain_senslope'].values[0] != None:
+            gauge_ids += [site_rain_props['rain_senslope'].values[0]]
     except:
         pass
     try:
-        if rain_props['rain_arq'].values[0] != None:
-            gauge_ids += [rain_props['rain_arq'].values[0]]
+        if site_rain_props['rain_arq'].values[0] != None:
+            gauge_ids += [site_rain_props['rain_arq'].values[0]]
     except:
         pass
     # rain gauge id
     while True:
-        gauge = raw_input('rain gauge id (e.g. agbtaw or 557): ')
+        gauge = raw_input('rain gauge id (e.g. agbtaw or 557): ').lower()
         try:
             gauge = 'rain_noah_' + str(int(gauge))
         except:
             pass
         if gauge in gauge_ids:
             break
+        print '\n\n'
         print 'rain gauge id not in the list'
-        print ','.join(gauge_ids)
+        rain_lst = '###  ' + ','.join(gauge_ids) + '  ###'
+        print '#'*len(rain_lst)
+        print rain_lst
+        print '#'*len(rain_lst)
+        print '\n\n'
         
     # rainfall data for the past 3 days
     start = ts - timedelta(3) - timedelta(hours=0.5)
@@ -78,8 +83,20 @@ if __name__ == "__main__":
             ts, site, one, halfmax, three, twoyrmax = main(ts=ts, site=site)
         else:
             break
+    one_cml = '1-day: ' + str(one) + ' mm (threshold: ' + str(halfmax) + ')'
+    three_cml = '3-day: ' + str(three) + ' mm (threshold: ' + str(twoyrmax) + ')'
+    space = len(three_cml) - len(one_cml)
     print '\n\n\n\n\n'
-    print '1-day:', one, 'mm ( threshold:', halfmax, ')'
-    print '3-day:', three, 'mm ( threshold:', twoyrmax, ')'
+    print '#'*(max(len(one_cml), len(three_cml))+6)
+    if len(three_cml) == len(one_cml):
+        print '#  ' + one_cml + '  #'
+        print '#  ' + three_cml + '  #'
+    elif len(three_cml) > len(one_cml):
+        print '#  ' + one_cml + ' '*space + '  #'
+        print '#  ' + three_cml + '  #'
+    else:
+        print '#  ' + one_cml + '  #'
+        print '#  ' + three_cml + ' '*space + '  #'
+    print '#'*(max(len(one_cml), len(three_cml))+6)
     print '\n\n\n\n\n'
     print "runtime = ", datetime.now()-start_time
