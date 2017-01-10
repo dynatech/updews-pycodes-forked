c4966eb211be8306f1031febc3e779684cdc20fa
ivyjean19 <diligivyjean@gmail.com>
2017-01-10 15:00:14 +0800

    Update querySenslopeDb.py
    KHR
    
    - changed the default of msgid and targetnode to 0.

--- Analysis/querySenslopeDb.py
+++ Analysis/querySenslopeDb.py
@@ -236,7 +236,7 @@ def PushDBDataFrame(df,table_name):
 #    df.ts = pd.to_datetime(df.ts)
 #    
 #    return df
-def GetRawAccelData(siteid = "", fromTime = "", toTime = "", maxnode = 40, msgid = "", targetnode ="", batt=0, returndb=True):
+def GetRawAccelData(siteid = "", fromTime = "", toTime = "", maxnode = 40, msgid = 0, targetnode =0, batt=0, returndb=True):
     if not siteid:
         raise ValueError('no site id entered')
         
@@ -244,7 +244,7 @@ def GetRawAccelData(siteid = "", fromTime = "", toTime = "", maxnode = 40, msgid
         PrintOut('Querying database ...')
 
     if (len(siteid) == 5):
-        if not msgid:
+        if (msgid == 0):
             query = " SELECT timestamp,'%s' as 'name',id,xvalue,yvalue,zvalue,batt  FROM senslopedb.%s"  % (siteid,siteid)
             
             targetnode_query = " WHERE id IN (SELECT node_id FROM senslopedb.node_accel_table WHERE site_name = '%s' and accel = 1)" %siteid 
