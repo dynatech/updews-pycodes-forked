c56528f9e70ecbeca7c46f4a5ce11a40f47e723d
ivy jean <diligivyjean@gmail.com>
2017-02-16 13:15:26 +0800

    -new file on laison accel version 1 filtered and 10ground measurement data

--- Liaison/accelfiteredVersion1.py
+++ Liaison/accelfiteredVersion1.py
@@ -19,14 +19,13 @@ site = sys.argv[1]
 fdate = sys.argv[2]
 tdate = sys.argv[3]
 nid = sys.argv[4]
-ms = sys.argv[5]
 engine = create_engine('mysql+pymysql://updews:october50sites@127.0.0.1/senslopedb')
-query = "SELECT timestamp,id,msgid,xvalue,yvalue,zvalue,batt FROM senslopedb.%s where id = '%s'and msgid ='%s' and timestamp between '%s ' and '%s'" % (site,nid,ms,fdate,tdate)
+query = "SELECT * FROM senslopedb.%s where timestamp between '%s ' and '%s' and id='%s'" % (site,fdate,tdate,nid)
 df = pd.io.sql.read_sql(query,engine)
-df.columns = ['ts','id','msgid','x','y','z','v']
+df.columns = ['ts','id','x','y','z','s']
 df['name'] = site
 df_filt = filterSensorData.applyFilters(df, orthof=True, rangef=True, outlierf=True)
 dfajson = df_filt.reset_index().to_json(orient='records',date_format='iso')
 dfajson = dfajson.replace("T"," ").replace("Z","").replace(".000","")
 print dfajson
-    
+        
