1799ff59db29057a1c90be47180f550c434ce684
skylaw11 <skysolidum@gmail.com>
2017-02-20 17:55:29 +0800

    Edited based on errors in running the testSOMSheatmap.py

--- Analysis/Soms/heatmap.py
+++ Analysis/Soms/heatmap.py
@@ -31,6 +31,7 @@ from datetime import timedelta
 #t_win = raw_input('select monitoring window[1d, 3d, 30d]: ').lower()
 
 def heatmap(col, t_timestamp, t_win = '1d', is_debug = False):
+	
 	df_merge = pd.DataFrame()
 	smin=0; smax=255;mini = 0; maxi = 1300
 	
@@ -50,40 +51,45 @@ def heatmap(col, t_timestamp, t_win = '1d', is_debug = False):
 	
 	t_timestamp = pd.to_datetime(pd.to_datetime(t_timestamp) + timedelta(hours = 24))
 	f_timestamp = pd.to_datetime(pd.to_datetime(t_timestamp) - timedelta(hours = timew))	
-	
-	query = "select num_nodes from senslopedb.site_column_props where name = '%s'" %col
-	node = qs.GetDBDataFrame(query)
-	for node_num in range (1,int(node.num_nodes[0])+1):
-		df = CSR.getsomscaldata(col,node_num,f_timestamp,t_timestamp)
-	
-		df = df.reset_index()
-		df.ts=pd.to_datetime(df.ts)
-	
-		df.index=df.ts                         
-		df.drop('ts', axis=1, inplace=True)    
-	
-		df=df[((df<1300) == True) & ((df>0)==True)] 
-		df['cval'] = df['mval1'].apply(lambda x:(x- mini) * smax / (maxi) + smin)
-		dfrs =pd.rolling_mean(df.resample(interval), window=3, min_periods=1)   #mean for one day (dataframe)
-		dfrs = dfrs.drop('mval1', axis=1)
-		pd.options.display.float_format = '{:,.0f}'.format
-		
-		n=len(dfrs)-1
-	
-		dfp=dfrs[n-timew:n]
-		dfp = dfp.reset_index()
-	
-	
-	
-		
-		df_merge = pd.concat([df_merge, dfp], axis = 0)
+	if (len(col)<=4):
+		print "No calib data for V1"
+	else:
 		
 	
+		query = "select num_nodes from senslopedb.site_column_props where name = '%s'" %col
+		
+		node = qs.GetDBDataFrame(query)
+		for node_num in range (1,int(node.num_nodes[0])+1):
+			
+			df = CSR.getsomscaldata(col,node_num,f_timestamp,t_timestamp)
+			if (df.empty == True):
+				return "No Soil Moisture Data"
+			
+				
+			else:
+				df = df.reset_index()
+				df.ts=pd.to_datetime(df.ts)
+			
+				df.index=df.ts                         
+				df.drop('ts', axis=1, inplace=True)    
+			
+				df=df[((df<1300) == True) & ((df>0)==True)] 
+				df['cval'] = df['mval1'].apply(lambda x:(x- mini) * smax / (maxi) + smin)
+				dfrs =pd.rolling_mean(df.resample(interval), window=3, min_periods=1)   #mean for one day (dataframe)
+				if 'mval1' in df.columns:				
+					dfrs = dfrs.drop('mval1', axis=1)
+				else:
+					pd.options.display.float_format = '{:,.0f}'.format
+					
+					n=len(dfrs)-1
+				
+					dfp=dfrs[n-timew:n]
+					dfp = dfp.reset_index()
+				
+					df_merge = pd.concat([df_merge, dfp], axis = 0)
 	
-	
-	
-	
-	df_merge['ts'] = df_merge.ts.astype(str)
-	dfjson = df_merge.to_json(orient='records' , double_precision=0)
-	
-	return dfjson
+			
+					df_merge['ts'] = df_merge.ts.astype(str)
+					dfjson = df_merge.to_json(orient='records' , double_precision=0)
+				
+					return dfjson
