a5a1d0a124cbfe5bcef5d8cef9950752f79a27a7
skylaw11 <skysolidum@gmail.com>
2017-03-02 14:55:36 +0800

    Fix minor bug for soms output - edited output to mval1

--- Analysis/Soms/ConvertSomsRaw.py
+++ Analysis/Soms/ConvertSomsRaw.py
@@ -33,6 +33,7 @@ def getsomsrawdata(column="", gid=0, fdate="", tdate="", if_multi = False):
     v3=[ 'lpasa','lpasb','laysa','laysb','imesb','barsc','messb','imusc','oslsc',
          'mngsa','gaasa','gaasb','hinsa','hinsb','talsa' ]
     df = pd.DataFrame(columns=['sraw', 'scal'])
+    sraw = pd.DataFrame()
 #    print 'getsomsdata: ' + column + ',' + str(gid)
     try:
         df = qs.GetSomsData(siteid=column+'m', fromTime=fdate, toTime=tdate, targetnode=gid)
@@ -47,26 +48,27 @@ def getsomsrawdata(column="", gid=0, fdate="", tdate="", if_multi = False):
         if column.upper()=='NAGSA':
 		 if if_multi:
 			df = df[(df.msgid == 21)]	
-			df['output'] =(((8000000/(df.mval1))-(8000000/(df.mval2)))*4)/10
-			sraw = df[['id','mval1']]
+			sraw['mval1'] =(((8000000/(df.mval1))-(8000000/(df.mval2)))*4)/10
+			sraw['id'] = df[['id']]
+			
 		 else:
-			sraw =(((8000000/(df.mval1[(df.msgid==21)]))-(8000000/(df.mval2[(df.msgid==21)])))*4)/10
-					
+			sraw['mval'] =(((8000000/(df.mval1[(df.msgid==21)]))-(8000000/(df.mval2[(df.msgid==21)])))*4)/10
+			 		
         else:
 		if if_multi:
 			df = df[(df.msgid == 111)]
-			df['output'] =(((20000000/(df.mval1))-(20000000/(df.mval2)))*4)/10    
-			sraw = df[['id','output']]   
+			sraw['mval1'] =(((20000000/(df.mval1))-(20000000/(df.mval2)))*4)/10    
+			sraw['id'] = df[['id']]
 		else:
-			sraw =(((20000000/(df.mval1[(df.msgid==111)]))-(20000000/(df.mval2[(df.msgid==111)])))*4)/10 
+			sraw['mval1'] =(((20000000/(df.mval1[(df.msgid==111)]))-(20000000/(df.mval2[(df.msgid==111)])))*4)/10 
 			
     elif column.lower() in v3: # if version 3
         if if_multi:
 		df = df[(df.msgid == 110)]
 		sraw = df[['id','mval1']]
         else:
-		sraw=df.mval1[(df.msgid==110)]
-		
+		df = df[(df.msgid==110)]
+		sraw = df[['mval1']]
 		
     else:
         sraw=pd.Series()
@@ -109,7 +111,7 @@ def getsomscaldata(column="", gid=0, fdate="", tdate="",is_debug= False, if_mult
         if if_multi:
 	        df = df[['id','mval1']]
         else:
-		  df= df.mval1
+		  df= df[['mval1']]
 	          
 	   
 								
