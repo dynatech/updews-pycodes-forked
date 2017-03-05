7cf7b124d6966e3d81f08cef946d526115b5f6cd
ivy jean <diligivyjean@gmail.com>
2017-03-06 02:18:54 +0800

    - python file for heatmap and updated code for soms filter

---
+++ Liaison/heatmap-visual.py
@@ -0,0 +1,17 @@
+import os
+import sys
+
+path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..//Analysis//Soms//'))
+if not path in sys.path:
+   sys.path.insert(1, path)
+del path
+import heatmap as htmap
+
+site = sys.argv[1]
+tdate = sys.argv[2]
+days = sys.argv[3]
+
+#site = 'mngsa'
+#tdate = '2016-08-01'
+#days = '1d'
+data = htmap.heatmap(site, '2016-08-01 00:00:00' , t_win = days)
