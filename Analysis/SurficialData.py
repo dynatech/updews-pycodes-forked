c87a5d4f6e54b03a5cb43f385943d425a5f04da4
kennexrazon <kennexrazon@gmail.com>
2017-03-06 16:15:35 +0800

    KHR
    - renamed the tables used in accordance with the agreed upon table names
    - changed return type of FindReplaceNull() from None to 'null'

--- Analysis/SurficialData.py
+++ Analysis/SurficialData.py
@@ -31,7 +31,7 @@ path = os.path.dirname(os.path.realpath(__file__))
 ###HARD CODED
 surficial_data_path = path + '/SurficialDataFiles/'
 
-def CreateSurficialTable(table_name):
+def CreateSurficialTable(table_name,verbose=False):
     #### Create Surficial Database table with name table_name
 
     db = mysqlDriver.connect(host = config.dbio.hostdb, user = config.dbio.userdb, passwd = config.dbio.passdb)
@@ -39,26 +39,36 @@ def CreateSurficialTable(table_name):
 
     cur.execute("USE {}".format(config.dbio.namedb))
     
-    if table_name == 'surficial_markers':
-        cur.execute("CREATE TABLE IF NOT EXISTS {}(marker_id int AUTO_INCREMENT, description varchar (50), latitude decimal (9, 6), longitude decimal (9, 6), in_use boolean, site_id tinyint unsigned, PRIMARY KEY (marker_id), FOREIGN KEY (site_id) REFERENCES sites(site_id))".format(table_name))
+    if table_name == 'markers': #tapos na
+        cur.execute("CREATE TABLE IF NOT EXISTS {}(marker_id smallint(6) AUTO_INCREMENT, description varchar (50), latitude decimal (9, 6), longitude decimal (9, 6), in_use boolean, site_id tinyint unsigned, PRIMARY KEY (marker_id), FOREIGN KEY (site_id) REFERENCES sites(site_id))".format(table_name))
+        if verbose:
+            print ' markers table CREATED / ALREADY EXISTS '
         db.close()
-    elif table_name == 'surficial_observations':
-        cur.execute("CREATE TABLE IF NOT EXISTS {}(so_id int AUTO_INCREMENT, ts timestamp, meas_type varchar (10), observer_name varchar (50), data_source varchar (4), reliability boolean, weather varchar(10), PRIMARY KEY (so_id))".format(table_name))
+    elif table_name == 'marker_observations': #tapos na
+        cur.execute("CREATE TABLE IF NOT EXISTS {}(observation_id int AUTO_INCREMENT, ts timestamp, meas_type varchar (10), observer_name varchar (50), data_source varchar (4), reliability boolean, weather varchar(10), PRIMARY KEY (observation_id))".format(table_name))
+        if verbose:   
+            print ' marker_observations table CREATED / ALREADY EXISTS '        
         db.close()
-    elif table_name == 'surficial_data':
-        cur.execute("CREATE TABLE IF NOT EXISTS {}(sd_id int AUTO_INCREMENT, marker_id int, so_id int, meas float, PRIMARY KEY (sd_id), FOREIGN KEY (marker_id) REFERENCES surficial_markers (marker_id), FOREIGN KEY (so_id) REFERENCES surficial_observations (so_id))".format(table_name))
+    elif table_name == 'marker_data': #tapos na
+        cur.execute("CREATE TABLE IF NOT EXISTS {}(data_id int AUTO_INCREMENT, measurement float, marker_id smallint(6),  observation_id int, PRIMARY KEY (data_id), FOREIGN KEY (marker_id) REFERENCES markers (marker_id), FOREIGN KEY (observation_id) REFERENCES marker_observations (observation_id))".format(table_name))
+        if verbose:   
+            print ' marker_data table CREATED / ALREADY EXISTS ' 
         db.close()
-    elif table_name == 'marker_history':
-        cur.execute("CREATE TABLE IF NOT EXISTS {}(history_id int AUTO_INCREMENT, marker_id int, ts timestamp, operation varchar(20), PRIMARY KEY (history_id), FOREIGN KEY (marker_id) REFERENCES surficial_markers (marker_id))".format(table_name))
+    elif table_name == 'marker_history': #tapos na
+        cur.execute("CREATE TABLE IF NOT EXISTS {}(history_id int AUTO_INCREMENT, marker_id smallint(6), ts timestamp, event varchar(20), PRIMARY KEY (history_id), FOREIGN KEY (marker_id) REFERENCES markers (marker_id))".format(table_name))
+        if verbose:   
+            print ' marker_history table CREATED / ALREADY EXISTS ' 
         db.close()
     elif table_name == 'marker_names':
-        cur.execute("CREATE TABLE IF NOT EXISTS {}(mn_id int AUTO_INCREMENT, history_id int, marker_name varchar (20), PRIMARY KEY (mn_id), FOREIGN KEY (history_id) REFERENCES marker_history (history_id))".format(table_name))
+        cur.execute("CREATE TABLE IF NOT EXISTS {}(name_id int(11) AUTO_INCREMENT, history_id int, marker_name varchar (20), PRIMARY KEY (name_id), FOREIGN KEY (history_id) REFERENCES marker_history (history_id))".format(table_name))
+        if verbose:   
+            print ' marker_names table CREATED / ALREADY EXISTS ' 
         db.close()
 
 
 def CreateAllSurficialTables():
     #### Creates all Surficial Database Tables in correct order
-    for table_name in ['surficial_markers','surficial_observations','surficial_data','marker_history','marker_names']:
+    for table_name in ['markers','marker_observations','marker_data','marker_history','marker_names']:
         CreateSurficialTable(table_name)
 
 def GetSiteID(code):
@@ -66,7 +76,7 @@ def GetSiteID(code):
     cur = db.cursor()
     cur.execute("USE {}".format(config.dbio.namedb))
     
-    cur.execute('SELECT site_id FROM sites WHERE code = "{}"'.format(code))
+    cur.execute('SELECT site_id FROM sites WHERE site_code = "{}"'.format(code))
     
     try:
         site_id = cur.fetchone()[0]
@@ -85,12 +95,12 @@ def GetMarkerID(site_id,marker_name = None,lat_long = None):
         print "ERROR specify lat long or marker name"
     else:
         try:
-            cur.execute('SELECT surficial_markers.marker_id FROM surficial_markers INNER JOIN marker_history ON surficial_markers.site_id = {} AND marker_history.marker_id = surficial_markers.marker_id INNER JOIN marker_names ON marker_history.history_id = marker_names.history_id AND marker_names.marker_name = "{}"'.format(site_id,marker_name))
+            cur.execute('SELECT markers.marker_id FROM markers INNER JOIN marker_history ON markers.site_id = {} AND marker_history.marker_id = markers.marker_id INNER JOIN marker_names ON marker_history.history_id = marker_names.history_id AND marker_names.marker_name = "{}"'.format(site_id,marker_name))
             marker_id = cur.fetchone()[0]
             db.close()
             return marker_id
         except:
-            cur.execute('SELECT surficial_markers.marker_id FROM surficial_markers WHERE surficial_markers.site_id = {} AND surficial_markers.latitude = {} AND surficial_markers.longitude = {}'.format(site_id,lat_long[0],lat_long[1]))
+            cur.execute('SELECT markers.marker_id FROM markers WHERE markers.site_id = {} AND markers.latitude = {} AND markers.longitude = {}'.format(site_id,lat_long[0],lat_long[1]))
             marker_id = cur.fetchone()[0]
             db.close()
             return marker_id
@@ -115,7 +125,7 @@ def InsertRenamedMarkers(renamed_markers):
     cur.execute("USE {}".format(config.dbio.namedb))       
     
     try:
-        cur.execute('SELECT surficial_markers.marker_id FROM surficial_markers INNER JOIN marker_history ON surficial_markers.site_id = {} AND marker_history.marker_id = surficial_markers.marker_id INNER JOIN marker_names ON marker_history.history_id = marker_names.history_id AND marker_names.marker_name = "{}"'.format(site_id,marker_name))
+        cur.execute('SELECT markers.marker_id FROM markers INNER JOIN marker_history ON markers.site_id = {} AND marker_history.marker_id = markers.marker_id INNER JOIN marker_names ON marker_history.history_id = marker_names.history_id AND marker_names.marker_name = "{}"'.format(site_id,marker_name))
 
         marker_id = cur.fetchone()[0]
         print "Marker {} for site {} exists in database".format(marker_name,code.upper())
@@ -123,7 +133,7 @@ def InsertRenamedMarkers(renamed_markers):
         marker_id = GetMarkerID(site_id,previous_name,lat_long)
  
         
-        cur.execute('INSERT INTO marker_history(marker_id,ts,operation) VALUES ({},"{}","{}")'.format(marker_id,pd.to_datetime(renamed_markers.ts.values[0]),renamed_markers.operation.values[0]))
+        cur.execute('INSERT INTO marker_history(marker_id,ts,event) VALUES ({},"{}","{}")'.format(marker_id,pd.to_datetime(renamed_markers.ts.values[0]),renamed_markers.operation.values[0]))
         db.commit()
         cur.execute('INSERT INTO marker_names(history_id,marker_name) VALUES(@@IDENTITY,"{}")'.format(marker_name))
         db.commit()
@@ -139,7 +149,7 @@ def InsertNewMarkers(new_markers):
     latitude = new_markers.latitude.values[0]
     longitude = new_markers.longitude.values[0]
     #### Check for null values
-    if not(description == description):
+    if not(description == description): #pd.isnull(description)
         description = 'No description'
     if not(latitude == latitude):
         latitude = 'null'
@@ -152,15 +162,15 @@ def InsertNewMarkers(new_markers):
     cur = db.cursor()
     cur.execute("USE {}".format(config.dbio.namedb))    
     
-    cur.execute('SELECT marker_names.marker_name FROM surficial_markers INNER JOIN marker_history ON surficial_markers.site_id = {} AND marker_history.marker_id = surficial_markers.marker_id INNER JOIN marker_names ON marker_history.history_id = marker_names.history_id WHERE marker_name = "{}"'.format(site_id,marker_name))
+    cur.execute('SELECT marker_names.marker_name FROM markers INNER JOIN marker_history ON markers.site_id = {} AND marker_history.marker_id = markers.marker_id INNER JOIN marker_names ON marker_history.history_id = marker_names.history_id WHERE marker_name = "{}"'.format(site_id,marker_name))
     
     try:
         marker_name = cur.fetchone()[0]
         print "Marker {} for site {} exists in database".format(marker_name,code.upper())
     except:
-        cur.execute('INSERT INTO surficial_markers (description, latitude, longitude, in_use, site_id) VALUES ("{}",{},{},1,{})'.format(description,latitude,longitude,site_id))
+        cur.execute('INSERT INTO markers (description, latitude, longitude, in_use, site_id) VALUES ("{}",{},{},1,{})'.format(description,latitude,longitude,site_id))
         db.commit()
-        cur.execute('INSERT INTO marker_history(marker_id,ts,operation) VALUES (@@IDENTITY,"{}","{}")'.format(pd.to_datetime(new_markers.ts.values[0]),new_markers.operation.values[0]))
+        cur.execute('INSERT INTO marker_history(marker_id,ts,event) VALUES (@@IDENTITY,"{}","{}")'.format(pd.to_datetime(new_markers.ts.values[0]),new_markers.operation.values[0]))
         db.commit()
         cur.execute('INSERT INTO marker_names(history_id,marker_name) VALUES(@@IDENTITY,"{}")'.format(marker_name))
         db.commit()        
@@ -175,7 +185,7 @@ def GetMarkerIDArray(df):
     
 def FindReplaceNull(x):
     if not(x==x):
-        return None
+        return 'null'
     else:
         return x
 
@@ -189,18 +199,18 @@ def InsertOtherHistories(other_histories):
     cur.execute("USE {}".format(config.dbio.namedb))    
     
     #### Check for duplicates
-    cur.execute('SELECT marker_history.history_id FROM marker_history WHERE ts = "{}" AND marker_id = {} AND operation = "{}"'.format(ts,marker_id,operation))
+    cur.execute('SELECT marker_history.history_id FROM marker_history WHERE ts = "{}" AND marker_id = {} AND event = "{}"'.format(ts,marker_id,operation))
     
     try:
         history_id = cur.fetchone()[0]
         print "Entry {} for {} of Marker {} in {} already exists in the database with id {}.".format(ts.strftime('%Y-%m-%d %H:%M:%S'),operation,other_histories.marker_name.values[0],other_histories.code.values[0],history_id)
     except:
-        cur.execute('INSERT INTO marker_history(marker_id,ts,operation) VALUES ({},"{}","{}")'.format(marker_id,ts,operation))
+        cur.execute('INSERT INTO marker_history(marker_id,ts,event) VALUES ({},"{}","{}")'.format(marker_id,ts,operation))
         db.commit()
         
         #### Kill deactivated markers
         if operation == 'deactivate':
-            cur.execute('UPDATE surficial_markers SET in_use = 0 WHERE marker_id = {}'.format(marker_id))
+            cur.execute('UPDATE markers SET in_use = 0 WHERE marker_id = {}'.format(marker_id))
             db.commit()
 
         db.close()
@@ -232,7 +242,13 @@ def UpdateSurficialMarkers(csv_filename):
     print "Marker history data from {}.csv has been successfully integrated".format(csv_filename)
 
 def UpdateSurficialObservations(surficial_observations):
+#    file_path = surficial_data_path + surficial_observations + '.csv'
+#    surficial_observations = pd.read_csv(file_path)
+#
+#    surficial_observations.columns = ['site_code','marker_name','ts','meas_type','observer_name','weather','data_source','meas','reliability']
+#
 #    surficial_observations = surficial_observations.applymap(FindReplaceNull)
+
     ts = pd.to_datetime(surficial_observations.ts.values[0])
     meas_type = surficial_observations.meas_type.values[0]
     observer_name = surficial_observations.observer_name.values[0]
@@ -243,23 +259,23 @@ def UpdateSurficialObservations(surficial_observations):
     db = mysqlDriver.connect(host = config.dbio.hostdb, user = config.dbio.userdb, passwd = config.dbio.passdb)
     cur = db.cursor()
     cur.execute("USE {}".format(config.dbio.namedb))    
-    
-    cur.execute('SELECT surficial_observations.so_id FROM surficial_observations WHERE ts = "{}" AND meas_type = "{}" AND observer_name = "{}" AND reliability = {} AND weather = "{}" AND data_source = "{}"'.format(ts,meas_type,observer_name,reliability,weather,data_source))    
-    
+#    
+    cur.execute('SELECT marker_observations.observation_id FROM marker_observations WHERE ts = "{}" AND meas_type = "{}" AND observer_name = "{}" AND reliability = {} AND weather = "{}" AND data_source = "{}"'.format(ts,meas_type,observer_name,reliability,weather,data_source))    
+#    
     try:
         so_id =cur.fetchone()[0]
         print "Duplicate entry check so_id = {}".format(so_id)
     except:
-        cur.execute('INSERT INTO surficial_observations(ts,meas_type,observer_name,reliability,weather,data_source) VALUES ("{}","{}","{}",{},"{}","{}")'.format(ts,meas_type,observer_name,reliability,weather,data_source))
+        cur.execute('INSERT INTO marker_observations(ts,meas_type,observer_name,reliability,weather,data_source) VALUES ("{}","{}","{}",{},"{}","{}")'.format(ts,meas_type,observer_name,reliability,weather,data_source))
         db.commit()
         surficial_observations.loc[:,'marker_id'] = surficial_observations.apply(GetMarkerIDArray,axis = 1)
         
-        cur.execute('SELECT surficial_observations.so_id FROM surficial_observations WHERE ts = "{}" AND meas_type = "{}" AND observer_name = "{}" AND reliability = {} AND weather = "{}" AND data_source = "{}"'.format(ts,meas_type,observer_name,reliability,weather,data_source))    
+        cur.execute('SELECT marker_observations.observation_id FROM marker_observations WHERE ts = "{}" AND meas_type = "{}" AND observer_name = "{}" AND reliability = {} AND weather = "{}" AND data_source = "{}"'.format(ts,meas_type,observer_name,reliability,weather,data_source))    
         so_id =cur.fetchone()[0]
         surficial_observations['so_id'] = so_id
         values = [tuple(x) for x in surficial_observations[['marker_id','meas','so_id']].values]
         print values
-        cur.executemany('INSERT INTO surficial_data(marker_id,meas,so_id) VALUES(%s,%s,%s)',values)        
+        cur.executemany('INSERT INTO marker_data(marker_id,measurement,observation_id) VALUES(%s,%s,%s)',values)        
         db.commit()
         db.close()
         
