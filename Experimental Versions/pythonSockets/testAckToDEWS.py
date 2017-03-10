34ec90912860b86ca8bfb7593de382f9f8823232
Updews Prado <updews.prado@gmail.com>
2017-03-10 18:47:02 +0800

    - dewsSocketLeanLib.py->sendBatchAckGSMtoDEWS(): corrected print message from function - testAckToDEWS.py and testInboxToDEWS.py, created more understandable comments for the users

--- Experimental Versions/pythonSockets/testAckToDEWS.py
+++ Experimental Versions/pythonSockets/testAckToDEWS.py
@@ -11,5 +11,13 @@ port = 5050
 # host = "www.dewslandslide.com"
 host = "sandbox"
 
-# Send 100 messages from smsinbox to the websocket server at a time
+# Send ALL Acknowledgment messages of smsoutbox to the websocket server
+# 	Messages included here have "send_status" values of:
+# 	a. SENT
+# 	b. FAIL
+# 
+# Once messages have been sent to the chatterbox server, the "send_status"
+# 	will be updated to the following values:
+# 	a. SENT -> SENT-WSS
+#	b. FAIL -> FAIL-WSS
 dsll.sendAllAckGSMToDEWS(host, port)
