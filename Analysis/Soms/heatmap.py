544791a48dbf887925b1fc2c95405b04a0c3bddc
ivy jean dilig <diligivyjean@gmail.com>
2017-03-16 15:57:33 +0800

    Revert "- updated heatmap file path"
    This reverts commit d887f426f7dcdc8aa010f5ba316b2e0add472d6b.

--- Analysis/Soms/heatmap.py
+++ Analysis/Soms/heatmap.py
@@ -96,7 +96,7 @@ def heatmap(col, t_timestamp, t_win = '1d'):
            return 'v1'                     
 				
 	
-site = sys.argv[1]
-tdate = sys.argv[2]
-days = sys.argv[3]
+site = 'laysa'
+tdate = '2016-03-03'
+days = '1d'	
 heatmap(site, tdate, t_win = days)
--- Liaison/heatmap-visual.py
+++ Liaison/heatmap-visual.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../Analysis/Soms/'))
+path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..//Analysis//Soms//'))
 if not path in sys.path:
    sys.path.insert(1, path)
 del path
@@ -11,7 +11,7 @@ site = sys.argv[1]
 tdate = sys.argv[2]
 days = sys.argv[3]
 
-#site = 'laysa'
+#site = 'gaasa'
 #tdate = '2016-03-03'
 #days = '1d'
 data = htmap.heatmap(site, tdate , t_win = days)
