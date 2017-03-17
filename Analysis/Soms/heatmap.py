e3c8fb3aa46ad15343a2e2f76df8022275aeba84
ivy jean dilig <diligivyjean@gmail.com>
2017-03-17 11:38:46 +0800

    -add .replace on tdate variable in heatmap source visuals

--- Analysis/Soms/heatmap.py
+++ Analysis/Soms/heatmap.py
@@ -98,5 +98,5 @@ def heatmap(col, t_timestamp, t_win = '1d'):
 	
 site = sys.argv[1]
 tdate = sys.argv[2]
-days = sys.argv[3]	
+days = sys.argv[3].replace("T"," ")	
 heatmap(site, tdate, t_win = days)
