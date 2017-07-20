067b1b93d03112625610a7781306141a8d0e5b5f
eavmendoza <earl.mendoza@gmail.com>
2017-07-20 15:46:36 +0800

    eavm - changed locking of alertgenexec.py to memcache based from socket based because of errors in the lockscript.py code

--- localserver/alertgenexec.py
+++ localserver/alertgenexec.py
@@ -5,6 +5,7 @@ import subprocess
 import lockscript, time
 from datetime import datetime as dt
 
+mc = memcache.Client(['127.0.0.1:11211'],debug=0)
 
 def count_alert_analysis_instances():
 	p = subprocess.Popen("ps ax | grep alertgen.py -c", stdout=subprocess.PIPE, 
@@ -13,18 +14,13 @@ def count_alert_analysis_instances():
 	return int(out)
 
 def main():
-
-	lockscript.get_lock('alertgenexec')
-
 	print dt.today().strftime("%c")
 
 	c = cfg.config()
 
 	ongoing = []
 	
-	mc = memcache.Client(['127.0.0.1:11211'],debug=0)
-
-	proc_limit = c.io.proc_limit
+ 	proc_limit = c.io.proc_limit
 	
 	while True:
 		alertgenlist = mc.get('alertgenlist')
@@ -56,8 +52,20 @@ def main():
 
 		while count_alert_analysis_instances() > proc_limit:
 			time.sleep(5)
-			print '.',	
+			print '.',
+
 
 
 if __name__ == "__main__":
-    main()
+    ag_instance = mc.get('alertgenexec')
+    if ag_instance == True:
+        print 'Process already exists (via memcache).. aborting'
+        sys.exit()
+
+    mc.set('alertgenexec', True)
+    try:
+		main()
+    except:
+		print 'Unexpected Error'
+	    	
+    mc.set('alertgenexec', False)
