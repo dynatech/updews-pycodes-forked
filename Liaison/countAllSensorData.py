08de3788858aea623cbe6868cd4410d39bc0526a
ivy jean dilig <diligivyjean@gmail.com>
2017-09-15 11:04:12 +0800

    - update get all sensor table count and latest data

--- Liaison/countAllSensorData.py
+++ Liaison/countAllSensorData.py
@@ -1,17 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Sep 14 14:08:34 2017
-
-@author: USER
-"""
-
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Sep 14 14:01:15 2017
-
-@author: USER
-"""
-
 import pandas as pd
 import numpy as np
 from datetime import timedelta as td
@@ -26,17 +12,29 @@ import json
 engine = create_engine('mysql+pymysql://root:senslope@127.0.0.1/senslopedb')
 query = "SELECT name FROM senslopedb.site_column_props order by name asc"
 df = pd.io.sql.read_sql(query,engine)
-data_sets=[]
+all_data= []
 for site in df.name:
+   filtered_data =[]
    query_count = "select count(*) from senslopedb.%s"%(site)
    df_count = pd.io.sql.read_sql(query_count,engine)
    df_count.columns = ['count']
    df_count['site'] = site
    query_latest = "select timestamp from senslopedb.%s  order by  timestamp desc limit 1"%(site)
    df_latest = pd.io.sql.read_sql(query_latest,engine)
-   df_count['latest'] = df_latest 
-   dfajson = df_count.to_json(orient='records',date_format='iso')
-   dfajson = dfajson.replace("T"," ").replace("Z","").replace(".000","")
-   data_sets.append(dfajson)
+   filtered_data.append(site)
+   filtered_data.append(df_count['count'][0])
+   if len( df_latest['timestamp']) == 1:
+        filtered_data.append(df_latest['timestamp'][0])
+   else:
+        filtered_data.append('null')
+        
+   all_data.append(filtered_data)
+ 
+new_df = pd.DataFrame(all_data)
+new_df.columns = ['site','count','latest']
+new_df.to_csv('//var//www//htdocs//temp//data//sensor_data.csv')
+dfajson = new_df.reset_index().to_json(orient="records",date_format='iso')
+dfajson = dfajson.replace("T"," ").replace("Z","").replace(".000","")
+print dfajson
 
-print data_sets
+
