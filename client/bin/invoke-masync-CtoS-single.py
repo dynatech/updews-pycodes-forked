5dec6534e18e57c1bf6b56da2cae622062d1c641
Prado Bognot <updews.prado@gmail.com>
2017-10-09 15:50:18 +0800

    invoke Masync | Added timestamp on print for debugging

--- client/bin/invoke-masync-CtoS-single.py
+++ client/bin/invoke-masync-CtoS-single.py
@@ -28,7 +28,7 @@ rankOffset = 9
 
 if len(sys.argv) >= 2:
     table = sys.argv[1].lower()
-    print "UPDATING TABLE: %s, rankOffset: %s" % (table, rankOffset)
+    print "[%s] UPDATING TABLE: %s, rankOffset: %s" % (datetime.now(), table, rankOffset)
     
     # Call this function to update selected table and sync the data from the client
     #   machine to the Masync Web Socket Server (dewslandslide.com)
