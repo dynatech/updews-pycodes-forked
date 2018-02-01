32a004f25d948c26e71dcf57b95916e8836f70ed
Meryll Viernes <meryllviernes08@gmail.com>
2018-02-01 14:05:12 +0800

    - RainfallAlert.py
    rx rainfall alert evaluation

--- Analysis/RainfallAlert.py
+++ Analysis/RainfallAlert.py
@@ -164,6 +164,8 @@ def RainfallAlert(siterainprops, end, s):
     try:
         query = "SELECT * FROM senslopedb.site_level_alert WHERE site = '%s' AND source = 'public' AND alert != 'A0' ORDER BY timestamp DESC LIMIT 3" %name
         prev_PAlert = q.GetDBDataFrame(query)
+        
+        query = "SELECT * FROM senslopedb.site_level_alert WHERE site = '%s' AND source = 'public' AND updateTS >= '%s' ORDER BY timestamp DESC LIMIT 1" %(name, end - timedelta(hours=0.5))
         currAlert = prev_PAlert['alert'].values[0]
 
         if currAlert != 'A0':
