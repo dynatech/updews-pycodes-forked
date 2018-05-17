a643eb3bb2b42a74a916cf316a6eee7c0f469a1c
ivy jean dilig <diligivyjean@gmail.com>
2018-05-17 11:02:43 +0800

    - update static variable query_string to set_static_variable and add function format_data

--- variable/static.py
+++ variable/static.py
@@ -16,43 +16,52 @@ class Variableinfo:
 
 def dict_format(query_string,variable_info):
 
-    query_output = dynadb.read(query_string)
-    data_format = variable_info.format[:-1][1:].replace(":",",").replace("\'","")
+  query_output = dynadb.read(query_string)
+  data_format = variable_info.format[:-1][1:]
+  data_format = data_format.replace(":",",")
+  data_format = data_format.replace("\'","")
 
-    dict_output = {a: b for a , b in query_output}
+  dict_output = {a: b for a , b in query_output}
 
-    return dict_output
+  return dict_output
 
-def query_static():
+def set_static_variable(name = ""):
 
-    query = 'Select * from static_variables'
-    variables = dynadb.read(query = query, 
-        identifier = 'Get all static_variables')
+  query = "Select * from static_variables"
+
+  if name != "":
+    query += " where name = '%s'" %(name)
+
+  variables = dynadb.read(query = query, 
+      identifier = 'Set static_variables')
+
+  for data in variables:
+    format_data(data)
 
 
-    for data in variables:
+def format_data(variable):
 
-        variable_info = Variableinfo(data)
-        query_string = variable_info.query.replace("LECT ",
-            "LECT "+ variable_info.column_list +" ")
+    variable_info = Variableinfo(variable)
+    query_string = variable_info.query.replace(
+      'LECT ','LECT '+ variable_info.column_list +' ')
 
-        if variable_info.type == 'data_frame':
-           df_query_output = dynadb.df_read(query_string)
-           memory.set(variable_info.name, df_query_output)
-           print variable_info.name
+    if variable_info.type == 'data_frame':
+       df_query_output = dynadb.df_read(query_string)
+       memory.set(variable_info.name, df_query_output)
+       print variable_info.name
 
-        elif variable_info.type == 'dict':
-           data_dict = dict_format(query_string,variable_info)
-           memory.set(variable_info.name, data_dict)
-           print variable_info.name
+    elif variable_info.type == 'dict':
+       data_dict = dict_format(query_string,variable_info)
+       memory.set(variable_info.name, data_dict)
+       print variable_info.name
 
-        else:
-           df_query_output = dynadb.read(query_string)
-           memory.set(df_query_output, data_dict)
-           print variable_info.name
+    else:
+       df_query_output = dynadb.read(query_string)
+       memory.set(df_query_output, data_dict)
+       print variable_info.name
 
 def main():
-    query_static()
+  set_static_variable()
 
 if __name__ == "__main__":
-    main()
+  main()
