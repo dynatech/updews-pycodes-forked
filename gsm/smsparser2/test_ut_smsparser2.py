1cccd09e860f16e5aedbe8ac6395d51c15cb848f
eavmendoza <earl.mendoza@gmail.com>
2018-07-30 10:48:48 +0800

    Added lines to test v2 tilt only and tilt with soms parsing

--- gsm/smsparser2/test_ut_smsparser2.py
+++ gsm/smsparser2/test_ut_smsparser2.py
@@ -35,9 +35,27 @@ class TestModule(unittest.TestCase):
 
         self.assertTrue('Wrong message construction' in context.exception)
 
+    def test_v2_1(self):
+        lgr_msg = ("GAATC*y*250CFD3EEFC8F7C260CFB302003086270C004E4FF5F81280C05"
+            "4C6FE0F84*180727120150")
+        lgr_ts = "2018-07-27 11:59:33"
+        sms = smsclass.SmsInbox(inbox_id=12345,msg=lgr_msg,
+            sim_num="639171234567",ts=lgr_ts)
+        status = smsparser2.subsurface.v2(sms)
+        self.assertIsNotNone(status)
+
+    def test_v2_2(self):
+        lgr_msg = ("GAASA*x*0A0B0843D0F9F830B0B014270F8F840C0BFF32301C0810D0B04"
+            "0000E0F82*180727120225")
+        lgr_ts = "2018-07-27 11:59:33"
+        sms = smsclass.SmsInbox(inbox_id=12345,msg=lgr_msg,
+            sim_num="639171234567",ts=lgr_ts)
+        status = smsparser2.subsurface.v2(sms)
+        self.assertIsNotNone(status)
+
 def main():
     suite = unittest.TestLoader().loadTestsFromTestCase(TestModule)
-    unittest.TextTestRunner(verbosity=1).run(suite)
+    unittest.TextTestRunner(verbosity=2).run(suite)
 
 if __name__ == "__main__":
     main()
