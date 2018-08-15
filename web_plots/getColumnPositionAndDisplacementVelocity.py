ec1b88c60a81d70e9805c7f3eb52f4737b9d518d
ksdelacruz <kevindhaledelacruz@gmail.com>
2018-08-15 13:58:07 +0800

    - refactored several files for easy testing

--- web_plots/getColumnPositionAndDisplacementVelocity.py
+++ web_plots/getColumnPositionAndDisplacementVelocity.py
@@ -2,22 +2,17 @@ import sys
 from analysis.subsurface import vcdgen as vcd
 import pandas as pd
     
-def getDF():
-    
-    site_column = sys.argv[1]
-    end_ts = sys.argv[2].replace("n",'').replace("T"," ").replace("%20"," ")
-    start_ts = sys.argv[3].replace("n",'').replace("T"," ").replace("%20"," ")
+def get_vcd_data_json(site_column, end_ts, start_ts):
+    df = vcd.vcdgen(site_column, end_ts, start_ts) 
+    print "web_plots=" + df
 
+if __name__ == "__main__":
 #    site_column = "agbta"
 #    end_ts = "2017-11-11 06:00:00"
 #    start_ts = "2017-11-08 06:00:00"
     
-#    end_ts = pd.to_datetime(end_ts)
-#    start_ts = pd.to_datetime(start_ts)
+    site_column = sys.argv[1]
+    end_ts = sys.argv[2].replace("n",'').replace("T"," ").replace("%20"," ")
+    start_ts = sys.argv[3].replace("n",'').replace("T"," ").replace("%20"," ")
     
-#    print end_ts
-#    print start_ts
-    df = vcd.vcdgen(site_column, end_ts, start_ts) 
-    print "web_plots=" + df
-
-getDF()
+    get_vcd_data_json(site_column, end_ts, start_ts)
--- web_plots/getSurficialMarkerTrendingAnalysis.py
+++ web_plots/getSurficialMarkerTrendingAnalysis.py
@@ -3,23 +3,33 @@ from analysis.surficial import markeralerts as ma
 import json
 import querydb as qdb
 
-sc = qdb.memcached()
-
-#site_id = 1
-#marker_id = 197
-#ts = "2018-05-09 00:00:00"
-
-site_id = int(sys.argv[1])
-marker_id = int(sys.argv[2])
-ts = sys.argv[3].replace("n",'').replace("T"," ").replace("%20"," ")
-
-def get_marker_trending_analysis_json(site_id, marker_id, ts):
+def get_marker_trending_analysis_df(site_id, marker_id, ts):
+    sc = qdb.memcached()
     num_pts = sc['surficial']['surficial_num_pts']
     
     surficial_data_df = ma.get_surficial_data(site_id,ts,num_pts)
     marker_data_df = surficial_data_df[surficial_data_df.marker_id==marker_id]
-    return_json = ma.evaluate_trending_filter(marker_data_df,to_plot=False,to_json=True)
+    return_df = ma.evaluate_trending_filter(marker_data_df,to_plot=False,to_json=True)
     
-    print "web_plots=" + json.dumps(return_json)
+    return return_df
 
-get_marker_trending_analysis_json(site_id, marker_id, ts)
+def get_marker_trending_analysis_json(site_id, marker_id, ts):
+    return_df = get_marker_trending_analysis_df(site_id, marker_id, ts)
+    return_json = "web_plots=" + json.dumps(return_df)
+    
+    return return_json
+
+def main(site_id, marker_id, ts):
+    return_json = get_marker_trending_analysis_json(site_id, marker_id, ts)
+    print return_json
+
+if __name__ == "__main__":
+    #site_id = 1
+    #marker_id = 197
+    #ts = "2018-05-09 00:00:00"
+    
+    site_id = int(sys.argv[1])
+    marker_id = int(sys.argv[2])
+    ts = sys.argv[3].replace("n",'').replace("T"," ").replace("%20"," ")
+    
+    main(site_id, marker_id, ts)
