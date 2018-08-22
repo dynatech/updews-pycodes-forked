bfbfcb0394114f7ab114ea640a8ff00d9ffc7502
jhndvdgvr <david063095@gmail.com>
2018-08-22 17:33:47 +0800

    refactored functions of unit test for subsurface

--- test/test_getColumnPositionAndDisplacementVelocity.py
+++ test/test_getColumnPositionAndDisplacementVelocity.py
@@ -1,42 +1,46 @@
 from web_plots import getColumnPositionAndDisplacementVelocity as subsurface
 import unittest
 import pandas
-
+import sys
 
 class TestGetColumnPositionAndDisplacementVelocity(unittest.TestCase):
     unittest.TestCase.maxDiff = None
     
-    def test_subsurface_return_json_pos1(self):
-        fp = open("subsurface_return_1.json", "r")
-        expected_json = fp.read()
-        expected_json = expected_json[:-1]
-        return_json = subsurface.get_vcd_data_json(site_column="agbta",end_ts="2017-11-11 06:00:00", start_ts="2017-03-08 06:00:00")
+    def test_subsurface_return_json_true_positive_success_1(self):
+        file = open("subsurface_return_1.json", "r")
+        expected_json = file.read()
+        return_json = subsurface.get_vcd_data_json(site_column="agbta",\
+        	end_ts="2017-11-11 06:00:00", start_ts="2017-11-08 06:00:00")
+
+        self.assertMultiLineEqual(expected_json, return_json, "True positive success")
         
-        self.assertMultiLineEqual(expected_json, return_json, "True positive test failed")
-        
-    def test_subsurface_return_json_pos2(self):
+    def test_subsurface_return_json_true_positive_success_2(self):
         fp = open("subsurface_return_2.json", "r")
         expected_json = fp.read()
-        expected_json = expected_json[:-1]
-        return_json = subsurface.get_vcd_data_json(site_column="magta",end_ts="2017-07-11 06:00:00", start_ts="2017-07-08 06:00:00")
+        return_json = subsurface.get_vcd_data_json(site_column="magta",\
+        	end_ts="2017-07-11 06:00:00", start_ts="2017-07-08 06:00:00")
         
-        self.assertMultiLineEqual(expected_json, return_json, "True positive test failed")
+        self.assertMultiLineEqual(expected_json, return_json, "True positive success")
         
-    def test_subsurface_return_json_neg1(self):
+    def test_subsurface_return_json_true_negative_fail_1(self):
         expected_json = "false_string"
-        return_json = subsurface.get_vcd_data_json(site_column="magta",end_ts="2017-07-11 06:00:00", start_ts="2017-07-08 06:00:00")
+        return_json = subsurface.get_vcd_data_json(site_column="magta",\
+        	end_ts="2017-07-11 06:00:00", start_ts="2017-07-08 06:00:00")
         
-        self.assertMultiLineEqual(expected_json, return_json, "True negative test success")
+        self.assertNotEqual(expected_json, return_json, "True negative success")
         
-    def test_subsurface_return_json_neg2(self):
+    def test_subsurface_return_json_true_negative_fail_2(self):
         expected_json = "false_string"
-        return_json = subsurface.get_vcd_data_json(site_column="jorta",end_ts="2017-03-11 06:00:00", start_ts="2017-03-08 06:00:00")
+        return_json = subsurface.get_vcd_data_json(site_column="jorta",\
+        	end_ts="2017-03-11 06:00:00", start_ts="2017-03-08 06:00:00")
         
-        self.assertMultiLineEqual(expected_json, return_json, "True negative test failed")
+        self.assertNotEqual(expected_json, return_json, "True negative success")
         
     def test_subsurface_has_web_plots(self):
-        return_json = subsurface.get_vcd_data_json(site_column="agbta",end_ts="2017-11-11 06:00:00", start_ts="2017-03-08 06:00:00")
-        self.assertRegexMatches("web_plots=", return_json, "str `web_plots=` not found on return string")
+        return_json = subsurface.get_vcd_data_json(site_column="agbta",\
+        	end_ts="2017-11-11 06:00:00", start_ts="2017-03-08 06:00:00")
+
+        self.assertIn("web_plots=", return_json)
         
 def main():
 	suite = unittest.TestLoader().loadTestsFromTestCase(TestGetColumnPositionAndDisplacementVelocity)
