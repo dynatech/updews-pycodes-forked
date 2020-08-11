58291fbd427ae53f2036079bf7110bd2e1f9d06e
merylljv <meryllviernes08@gmail.com>
2020-08-11 19:27:02 +0800

    Update smsparser.py
    bug fixed on parsing marker measurement with recycled marker name

--- gsm/smsparser.py
+++ gsm/smsparser.py
@@ -261,7 +261,7 @@ def process_surficial_observation(sms):
 
         return False
 
-    site_surf_mark = surf_mark[surf_mark["site_id"] == obv["obv"]["site_id"]]
+    site_surf_mark = surf_mark[(surf_mark["site_id"] == obv["obv"]["site_id"]) & (surf_mark["in_use"] == 1)]
 
     df_meas = pd.DataFrame()
     df_meas = df_meas.from_dict(obv["markers"]["measurements"], orient = 'index')
