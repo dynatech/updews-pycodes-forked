cb9e03c99c2180f8ac2b2713b0fe3253c401f02d
merylljv <meryllviernes08@gmail.com>
2020-08-17 12:20:02 +0800

    Update eqevents.py

--- analysis/earthquake/eqevents.py
+++ analysis/earthquake/eqevents.py
@@ -1,16 +1,10 @@
 # -*- coding: utf-8 -*-
-### IMPORTANT matplotlib declarations must always be FIRST 
-### to make sure that matplotlib works with cron-based automation
-import platform
-if platform.system() == 'Linux':
-    import matplotlib as mpl
-    mpl.use('Agg')
-    import matplotlib.pyplot as plt
-    plt.ioff()
 
-from twitterscraper import query_tweets
+from tweepy import OAuthHandler
+from tweepy import API
+from tweepy import Cursor
 from datetime import datetime, timedelta
-
+import json
 import os
 import pandas as pd
 import re
@@ -21,54 +15,73 @@ import dynadb.db as db
 import gsm.smsparser2.smsclass as sms
 
 
-def get_eq_events(ts=datetime.now()):
-    tweets = query_tweets("@phivolcs_dost", begindate=(ts-timedelta(1)).date(),
-                          enddate=(ts+timedelta(1)).date())
+def get_auth():
+    """Authorize twitter access.
+    """
     
-    df = pd.DataFrame()
-    
-    for tweet in tweets:
-        if tweet.screen_name == "phivolcs_dost":
+    # Load credentials from json file
+    with open("twitter_credentials.json", "r") as file:
+    		creds = json.load(file)
+    # auth
+    auth = OAuthHandler(creds['consumer_key'], creds['consumer_secret'])
+    auth.set_access_token(creds['access_token'], creds['access_token_secret'])
+    auth_api = API(auth)
+    return auth_api
+
+
+def get_eq_events(text):
+    """Parse tweet text to get ts, magnitude, depth, latitude, longitude, 
+    province.
+    """
+
+    # timestamp
+    ts_re = r"(?<=Date and Time:)[ \:\-\w\d]*[AP]M"
+    ts_fmt = "%d %b %Y - %I:%M %p"
+    match = re.search(ts_re, text).group(0).strip()
+    ts = pd.to_datetime(datetime.strptime(match, ts_fmt))
+    # magnitude
+    mag_re = r"(?<=Magnitude =)[ \d\.]*"
+    mag = re.search(mag_re, text).group(0).strip()
+    # depth
+    depth_re = r"(?<=Depth =)[ \d\.]*(?=kilometers)"
+    depth = float(re.search(depth_re, text).group(0).strip())
+    # latitude and longitude
+    coord_re = r"(?<=Location =)([ \d\.]*)N[ \,]*([ \d\.]*)(?=E)"
+    match = re.search(coord_re, text)
+    lat = float(match.group(1))
+    lon = float(match.group(2))
+    # province
+    prov_re = r"(?<=Location =)([ \d\.\w\,\-\°]*)\(([ \w\)\(]*)(?=\))"
+    prov = re.search(prov_re, text).group(2).split('(')[-1].strip()
+    # consolidate into table
+    df = pd.DataFrame({'ts': [ts], 'magnitude': [mag], 'depth': [depth],
+                       'latitude': [lat], 'longitude': [lon],
+                       'province': [prov], 'issuer': ['dyna']})
+    return df
+
+
+def main(days=1):
+    auth_api = get_auth()
+    username = 'phivolcs_dost'
+    end_date = datetime.utcnow() - timedelta(days)
+
+    for status in Cursor(auth_api.user_timeline, id=username).items():
+        text = auth_api.get_status(status.id, tweet_mode="extended").full_text
+        if 'earthquake' in text.lower():
             try:
-                text = tweet.text
-        
-                # timestamp
-                ts_re = r"(?<=Date and Time:)[ \:\-\w\d]*[AP]M"
-                ts_fmt = "%d %b %Y - %I:%M %p"
-                match = re.search(ts_re, text).group(0).strip()
-                ts = str(datetime.strptime(match, ts_fmt))
-        
-                # magnitude
-                mag_re = r"(?<=Magnitude =)[ \d\.]*"
-                mag = float(re.search(mag_re, text).group(0).strip())
-        
-                # depth
-                depth_re = r"(?<=Depth =)[ \d\.]*(?=kilometers)"
-                depth = float(re.search(depth_re, text).group(0).strip())
-        
-                # latitude and longitude
-                coord_re = r"(?<=Location =)([ \d\.]*)N[ \,]*([ \d\.]*)(?=E)"
-                match = re.search(coord_re, text)
-                lat = float(match.group(1))
-                lon = float(match.group(2))
-                
-                # province
-                prov_re = r"(?<=Location =)([ \d\.\w\,\-\°]*)\(([ \w\)\(]*)(?=\))"
-                prov = str(re.search(prov_re, text).group(2).split('(')[-1].strip())
-                
-                df = df.append(pd.DataFrame({'ts': [ts], 'magnitude': [mag],
-                                             'depth': [depth],
-                                             'latitude': [lat],
-                                             'longitude': [lon],
-                                             'province': [prov],
-                                             'issuer': ['dyna']}),
-    ignore_index=True)
+                print(status.created_at)
+                df = get_eq_events(text)
+                data_table = sms.DataTable('earthquake_events', df)
+                db.df_write(data_table)
             except:
                 pass
-            
-    data_table = sms.DataTable('earthquake_events', df)
-    db.df_write(data_table)
     
-###############################################################################
-if __name__ == "__main__":
-    get_eq_events()
+        if status.created_at < end_date:
+            break
+
+##################################################
+if __name__ == '__main__':
+    start = datetime.now()
+    main()
+    runtime = datetime.now() - start
+    print('runtime =', runtime)
