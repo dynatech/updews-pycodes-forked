255c9e3f4c3b213ab0b357aefb92c4a28bf4278c
merylljv <meryllviernes08@gmail.com>
2020-09-14 17:02:36 +0800

    Update gsmalerts.py
    update for db 3

--- analysis/gsmalerts.py
+++ analysis/gsmalerts.py
@@ -8,7 +8,7 @@ import querydb as qdb
 import publicalerts as pub
 import gsm.gsmserver_dewsl3.sms_data as sms
 
-def site_alerts(curr_trig, ts, release_data_ts):
+def site_alerts(curr_trig, ts, release_data_ts, connection):
     site_id = curr_trig['site_id'].values[0]
 
     query = "SELECT site_id, stat.trigger_id, trigger_source, alert_level FROM "
@@ -19,23 +19,23 @@ def site_alerts(curr_trig, ts, release_data_ts):
     query += "  (SELECT trigger_id, site_id, trigger_source, alert_level FROM "
     query += "    (SELECT * FROM operational_triggers "
     query += "    WHERE site_id = %s " %site_id
-    query += "    ) as op "
+    query += "    ) AS op "
     query += "  INNER JOIN "
     query += "    (SELECT trigger_sym_id, trigger_source, alert_level FROM "
-    query += "      operational_trigger_symbols AS trig_sym "
+    query += "      .operational_trigger_symbols AS trig_sym "
     query += "    INNER JOIN "
-    query += "      trigger_hierarchies AS trig "
-    query += "    ON trig.source_id = trig_sym.source_id "
+    query += "      .trigger_hierarchies AS trig "
+    query += "    USING (source_id) "
     query += "    ) as sym "
-    query += "  ON op.trigger_sym_id = sym.trigger_sym_id "
+    query += "  USING (trigger_sym_id) "
     query += "  ) as sub "
-    query += "ON stat.trigger_id = sub.trigger_id"
-    sent_alert = qdb.get_db_dataframe(query)
+    query += "USING (trigger_id)"
+    sent_alert = db.df_read(query, connection=connection)
 
     query = "SELECT * FROM alert_status"
     query += " WHERE trigger_id in (%s)" %(','.join(map(lambda x: str(x), \
                                          set(curr_trig['trigger_id'].values))))
-    written = db.df_read(query)
+    written = db.df_read(query, connection=connection)
 
     site_curr_trig = curr_trig[~curr_trig.trigger_id.isin(written.trigger_id)]
     site_curr_trig = site_curr_trig.sort_values('alert_level', ascending=False)
@@ -63,9 +63,9 @@ def site_alerts(curr_trig, ts, release_data_ts):
             'ts_last_retrigger'})
     alert_status['ts_set'] = datetime.now()
     data_table = sms.DataTable('alert_status', alert_status)
-    db.df_write(data_table)
+    db.df_write(data_table, connection=connection)
 
-def main():
+def main(connection='analysis'):
     start_time = datetime.now()
     qdb.print_out(start_time)
     
@@ -88,11 +88,11 @@ def main():
     query += "    ) AS trig_sym "
     query += "  INNER JOIN "
     query += "    trigger_hierarchies AS trig "
-    query += "  ON trig_sym.source_id = trig.source_id "
+    query += "  USING (source_id) "
     query += "  ) AS sym "
-    query += "ON op.trigger_sym_id = sym.trigger_sym_id "
+    query += "USING (trigger_sym_id) "
     query += "ORDER BY ts_updated DESC"
-    curr_trig = db.df_read(query)
+    curr_trig = db.df_read(query, connection=connection)
 
     if len(curr_trig) == 0:
         qdb.print_out('no new trigger')
@@ -103,7 +103,7 @@ def main():
 
     curr_trig = curr_trig.rename(columns = {"ts_updated": "ts_last_retrigger"})
     site_curr_trig = curr_trig.groupby('site_id', as_index=False)
-    site_curr_trig.apply(site_alerts, ts=ts, release_data_ts=release_data_ts)
+    site_curr_trig.apply(site_alerts, ts=ts, release_data_ts=release_data_ts, connection=connection)
 
 ################################################################################
 
