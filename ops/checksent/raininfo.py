09fbbd31c48803d6e42e21a4c36b7cf4a37d8504
brainerdcruz <brainerd.cruz@gmail.com>
2021-10-19 06:42:29 +0800

    update raininfo.py

--- ops/checksent/raininfo.py
+++ ops/checksent/raininfo.py
@@ -18,15 +18,16 @@ def main(time_now = datetime.now(), mysql=True, to_csv=False):
         col_name = 'ts_sent'
     sent_sched = lib_raininfo.ewi_sched(start, end, mysql=mysql, to_csv=to_csv)
     
-    if len(sent_sched) != 0:
-            unqueued_unsent = sent_sched.loc[(sent_sched[col_name].isnull()) | (~sent_sched.unwritten_info.isnull()), ['rain_site_code', 'fullname', 'unwritten_info']].drop_duplicates()
-            msg = 'un{name} Rain Info ({ts})'.format(name=name, ts=start)
-            if len(unqueued_unsent) != 0:
-                msg += ':\n'
-                msg += '\n'.join(sorted(unqueued_unsent.apply(lambda row: (row.rain_site_code).upper() + ' ' + row.fullname + ' ' + row.unwritten_info, axis=1).values))
-            else:
-                msg = 'No ' + msg
-            msg = msg[0].upper() + msg[1:]
+    if len(sent_sched) != 0:        
+        unqueued_unsent = sent_sched.loc[(sent_sched[col_name].isnull()) | (~sent_sched.unwritten_info.isnull()) | (~sent_sched.tagged.isnull()), ['rain_site_code', 'fullname', 'unwritten_info', 'tagged']].drop_duplicates()
+        unqueued_unsent = unqueued_unsent.fillna('')
+        msg = 'un{name} Rain Info ({ts})'.format(name=name, ts=start)
+        if len(unqueued_unsent) != 0:
+            msg += ':\n'
+            msg += '\n'.join(sorted(unqueued_unsent.apply(lambda row: (row.rain_site_code).upper() + ' ' + row.fullname + ' ' + row.unwritten_info + ' ' + row.tagged, axis=1).values))
+        else:
+            msg = 'No ' + msg
+        msg = (msg[0].upper() + msg[1:]).replace('  ', ' ')
     else:
         msg = 'No scheduled rain info ({ts})'.format(ts=start)
     return msg
